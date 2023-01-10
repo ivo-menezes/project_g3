8ccdac3618f8040fd47914998f5020b94eb6ba64
Pedro Silva [1221855] <1221855@isep.ipp.pt>
2023-01-10 16:43:29 +0000

    US003: Implementation: attributes, constructors and methods of ProfileList and Services (incomplete)

--- src/main/java/org/switch2022/project/model/ProfileList.java
+++ src/main/java/org/switch2022/project/model/ProfileList.java
@@ -1,4 +1,26 @@
 package org.switch2022.project.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProfileList {
+    private List<Profile> profileList;
+
+    public ProfileList() {
+        this.profileList = new ArrayList<Profile>();
+    }
+
+    public Profile getProfile(String profileName) {
+        Profile profile = null;
+
+        for (int i = 0; i < this.profileList.size() && profile == null; i++) {
+            Profile p = this.profileList.get(i);
+            String pName = p.getProfileName();
+            if (pName.equals(profileName)) {
+                profile = p;
+            }
+        }
+
+        return profile;
+    }
 }
