cb54b56de97f728f58ebdebaf8f6037f25045cb6
Joana Fonseca [1221839] <1221839@isep.ipp.pt>
2023-01-10 10:54:45 +0000

    Classes: Implementation: Added attributes in Account Class

--- src/main/java/org/switch2022/project/model/Account.java
+++ src/main/java/org/switch2022/project/model/Account.java
@@ -1,4 +1,25 @@
 package org.switch2022.project.model;
 
 public class Account {
+    private String name;
+    private String email;
+    private String phone;
+    private String photo;
+    private Profile profile;
+    private boolean isActive = true;
+
+    public Account(String name, String email, String phone, Profile profile) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.profile = profile;
+    }
+
+    public Account(String name, String email, String phone, String photo, Profile profile) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.photo = photo;
+        this.profile = profile;
+    }
 }
