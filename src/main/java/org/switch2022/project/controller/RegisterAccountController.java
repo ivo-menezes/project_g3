ca6edf94126d294effaa0eeff33668b998b7e1ed
Pedro Silva [1221855] <1221855@isep.ipp.pt>
2023-01-13 20:17:45 +0000

    US002: Implementation: add RegisterAccountController + tests

--- src/main/java/org/switch2022/project/controller/RegisterAccountController.java
+++ src/main/java/org/switch2022/project/controller/RegisterAccountController.java
@@ -1,5 +1,25 @@
 package org.switch2022.project.controller;
 
-public class RegisterAccountController {
+import org.switch2022.project.model.AccountList;
+import org.switch2022.project.model.Profile;
+import org.switch2022.project.model.ProfileList;
+import org.switch2022.project.model.RegisterAccountDTO;
 
+public class RegisterAccountController {
+    private ProfileList profileList;
+    private AccountList accountList;
+
+    public RegisterAccountController(ProfileList profileList, AccountList accountList) {
+        this.profileList = profileList;
+        this.accountList = accountList;
+    }
+
+    public boolean registerAccount(RegisterAccountDTO dto) {
+        if (dto == null) {
+            throw new IllegalArgumentException("Account info (DTO) must not be null.");
+        }
+
+        Profile defaultProfile = profileList.getProfileByName("User");
+        return accountList.registerAccount(dto, defaultProfile);
+    }
 }
