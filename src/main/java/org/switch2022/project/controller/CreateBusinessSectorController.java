d83adb4572c3e713ce7f8464c64ec4b67fe4463c
Joana Fonseca [1221839] <1221839@isep.ipp.pt>
2023-01-19 20:40:04 +0000

    US008: Implementation: Added Javadocs and new tests.

--- src/main/java/org/switch2022/project/controller/CreateBusinessSectorController.java
+++ src/main/java/org/switch2022/project/controller/CreateBusinessSectorController.java
@@ -4,7 +4,13 @@ import org.switch2022.project.model.BusinessSectorList;
 
 public class CreateBusinessSectorController {
 
-    public CreateBusinessSectorController() {
+    private BusinessSectorList newBusinessList;
+    /***
+     * This controller will be used to create the business sector.
+     * The constructor doesn't receive anything, since it will be only pulled for the
+     */
+    public CreateBusinessSectorController(BusinessSectorList newBusinessList) {
+        this.newBusinessList = newBusinessList;
     }
 
     public boolean createBusinessSector(String designation) {
--- src/test/java/org/switch2022/project/controller/CreateBusinessSectorControllerTest.java
+++ src/test/java/org/switch2022/project/controller/CreateBusinessSectorControllerTest.java
@@ -1,30 +1,50 @@
 package org.switch2022.project.controller;
 
+
 import org.junit.jupiter.api.Test;
+import org.switch2022.project.model.BusinessSectorList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CreateBusinessSectorControllerTest {
 
+
+    @Test
+    void createdSuccessfully(){
+        BusinessSectorList newList = new BusinessSectorList();
+        CreateBusinessSectorController controller = new CreateBusinessSectorController(newList);
+        boolean resultedList = controller.createBusinessSector("Financial");
+
+        assertTrue(resultedList);
+        assertNotEquals(false, resultedList);
+    }
+
     @Test
     void createBusinessSector() {
         String designation = "Financial";
-        CreateBusinessSectorController controller = new CreateBusinessSectorController();
+        BusinessSectorList newList = new BusinessSectorList();
+        CreateBusinessSectorController controller = new CreateBusinessSectorController(newList);
 
         assertTrue(controller.createBusinessSector(designation));
     }
+
     @Test
     void checkIfItCreates(){
         String designation = "Financial";
-        CreateBusinessSectorController controller = new CreateBusinessSectorController();
+        BusinessSectorList newList = new BusinessSectorList();
+        CreateBusinessSectorController controller = new CreateBusinessSectorController(newList);
 
         assertTrue(controller.createBusinessSector(designation));
         assertNotEquals(false, controller.createBusinessSector(designation));
     }
+
+    /***
+     * Using a null to ensure the tests catch the error.
+     */
     @Test
     void checkIfItDesignationIsNotValidated(){
-
-        CreateBusinessSectorController controller = new CreateBusinessSectorController();
+        BusinessSectorList newList = new BusinessSectorList();
+        CreateBusinessSectorController controller = new CreateBusinessSectorController(newList);
 
         assertFalse(controller.createBusinessSector(null));
         assertNotEquals(true, controller.createBusinessSector(null));
