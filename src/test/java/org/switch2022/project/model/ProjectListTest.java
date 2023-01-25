da7a26542ef535fbfa47f26084f8c4d4fa1e8aff
Marisa Graça [1221849] <1221849@isep.ipp.pt>
2023-01-25 16:59:34 +0000

    US0014: Implementation: added tests to ProjectListTest

--- src/test/java/org/switch2022/project/model/ProjectListTest.java
+++ src/test/java/org/switch2022/project/model/ProjectListTest.java
@@ -3,7 +3,6 @@ package org.switch2022.project.model;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.switch2022.project.controller.ListProjectController;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -231,4 +230,33 @@ class ProjectListTest {
         assertNotNull(userProjectsDTO);
         assertSame(DTO1, DTO2);
     }
+
+    /*@Test
+    @DisplayName("get a list of resources as a list of DTOs")
+    void getAListOfResources() {
+        // arrange
+        // --Setup for project:
+        Project project = new Project(1, "test", "test");
+        Account account = new Account("Deborah", "deborah@hotmail.com", "938966782", new Profile("User"));
+        Account account1 = new Account("Pedro", "pedro@example.com", "9666666", new Profile("User"));
+        Role role = new Role("Team Member");
+        ResourceDTO resourceDTO = new ResourceDTO("deborah@hotmail.com", "Team Member", 1, new Date(), 25, 100);
+        ResourceDTO resourceDTO1 = new ResourceDTO("pedro@example.com", "Team Member", 1, new Date(),50,95);
+        project.addResource(account, role, resourceDTO);
+        project.addResource(account1, role, resourceDTO1);
+        // --Add project to a projectList:
+        ProjectList projectList = new ProjectList();
+        projectList.addProject(project);
+        // --Setup for the expected output:
+        List<ResourceDTO> listExpected = new ArrayList<>();
+        ResourceDTO resourceDTO_Output = new ResourceDTO("deborah@hotmail.com", "Team Member");
+        ResourceDTO resourceDTO1_Output = new ResourceDTO("pedro@example.com", "Team Member");
+        listExpected.add(resourceDTO_Output);
+        listExpected.add(resourceDTO1_Output);
+        // act
+        List<ResourceDTO> listResult =  projectList.listResources(1);
+        // assert
+        assertEquals(listExpected, listResult);
+    }*/
+
 }
