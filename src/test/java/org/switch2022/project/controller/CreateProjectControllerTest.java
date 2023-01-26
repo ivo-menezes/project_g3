8510b4d2737acec2dcbe4353dd8e7ab43ee06e7e
Tiago Silva [1221867] <1221867@isep.ipp.pt>
2023-01-26 09:37:23 +0000

    US010 : Implementation : added CreateProjectControllerTest

---
+++ src/test/java/org/switch2022/project/controller/CreateProjectControllerTest.java
@@ -0,0 +1,47 @@
+package org.switch2022.project.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.switch2022.project.model.Project;
+import org.switch2022.project.model.ProjectList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateProjectControllerTest {
+
+    @Test
+    @DisplayName("Ensure that project are created")
+    void createProjectSuccess() {
+        ProjectList projectList = new ProjectList();
+        CreateProjectController controller = new CreateProjectController(projectList);
+        boolean result = controller.createProject(1,"Project Test", "For test");
+        assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("Ensure that project not created and throws exception")
+    void createProjectWithoutSuccessAndThrowsException() {
+        ProjectList projectList = new ProjectList();
+        CreateProjectController controller = new CreateProjectController(projectList);
+        int code = 0;
+        String name = "Project Test";
+        String description = "For test";
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> controller.createProject(code, name, description));
+    }
+
+    @Test
+    @DisplayName("Ensure that project not created because already exists")
+    void createProjectWithoutSuccess() {
+        ProjectList projectList = new ProjectList();
+        Project projectTest = new Project(1,"Project Test", "For test");
+        projectList.addProject(projectTest);
+        CreateProjectController controller = new CreateProjectController(projectList);
+        int code = 1;
+        String name = "Project Test";
+        String description = "For test";
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> controller.createProject(code, name, description));
+    }
+}
