b4fcb4a0c6d4a372cc2ade4540f8db98fceba4c4
Pedro Silva [1221855] <1221855@isep.ipp.pt>
2023-03-10 14:36:18 +0000

    US017: Implementation: implement CreateUserStoryController + tests

---
+++ src/main/java/org/switch2022/project/controller/CreateUserStoryController.java
@@ -0,0 +1,27 @@
+package org.switch2022.project.controller;
+
+import org.switch2022.project.model.ProductBacklog;
+import org.switch2022.project.model.Project;
+import org.switch2022.project.model.ProjectList;
+import org.switch2022.project.model.UserStoryDTO;
+
+public class CreateUserStoryController {
+    private final ProjectList projectList;
+
+    public CreateUserStoryController(ProjectList projectList) {
+        if (projectList == null) {
+            throw new IllegalArgumentException("Project List must not be null");
+        }
+        this.projectList = projectList;
+    }
+
+    public boolean createUserStory(int projectCode, UserStoryDTO userStoryDTO, int priority) {
+        if (userStoryDTO == null) {
+            throw new IllegalArgumentException("UserStoryDTO must not be null");
+        }
+
+        Project project = projectList.getProject(projectCode);
+        ProductBacklog productBacklog = project.getProductBacklog();
+        return productBacklog.createAndAddUserStory(userStoryDTO, priority);
+    }
+}
---
+++ src/test/java/org/switch2022/project/controller/CreateUserStoryControllerTest.java
@@ -0,0 +1,110 @@
+package org.switch2022.project.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.switch2022.project.model.ProductBacklog;
+import org.switch2022.project.model.Project;
+import org.switch2022.project.model.ProjectList;
+import org.switch2022.project.model.UserStoryDTO;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class CreateUserStoryControllerTest {
+
+    @DisplayName("creating a controller with null project list throws exception")
+    @Test
+    void createControllerWithNullProjectListThrowsException() {
+        // Arrange
+        String expectedMessage = "Project List must not be null";
+        ProjectList projectList = null;
+        // Act
+        IllegalArgumentException result = assertThrows(IllegalArgumentException.class, () -> {
+            new CreateUserStoryController(projectList);
+        });
+        String resultMessage = result.getMessage();
+        // Assert
+        assertEquals(expectedMessage, resultMessage);
+    }
+
+    @DisplayName("creating a controller succeeds")
+    @Test
+    void createControllerSucceeds() {
+        // Arrange
+        ProjectList projectListDouble = mock(ProjectList.class);
+        // Act
+        CreateUserStoryController controller = new CreateUserStoryController(projectListDouble);
+        // Assert
+        assertInstanceOf(CreateUserStoryController.class, controller);
+    }
+
+    @DisplayName("create and add user story succeeds")
+    @Test
+    void createUserStorySucceedsWithIsolation() {
+        // Arrange
+        // create all necessary mock objects
+        ProjectList projectListDouble = mock(ProjectList.class);
+        Project projectDouble = mock(Project.class);
+        ProductBacklog productBacklogDouble = mock(ProductBacklog.class);
+        UserStoryDTO userStoryDTODouble = mock(UserStoryDTO.class);
+        // train the mock objects
+        when(projectListDouble.getProject(1)).thenReturn(projectDouble);
+        when(projectDouble.getProductBacklog()).thenReturn(productBacklogDouble);
+        when(productBacklogDouble.createAndAddUserStory(userStoryDTODouble, 0)).thenReturn(true);
+        // create a new (real) controller
+        CreateUserStoryController controller = new CreateUserStoryController(projectListDouble);
+
+        // Act
+        boolean result = controller.createUserStory(1, userStoryDTODouble, 0);
+
+        // Assert
+        assertTrue(result);
+    }
+
+    @DisplayName("create and add user story fails")
+    @Test
+    void createUserStoryFailsWithIsolation() {
+        // Arrange
+        // create all necessary mock objects
+        ProjectList projectListDouble = mock(ProjectList.class);
+        Project projectDouble = mock(Project.class);
+        ProductBacklog productBacklogDouble = mock(ProductBacklog.class);
+        UserStoryDTO userStoryDTODouble = mock(UserStoryDTO.class);
+        // train the mock objects
+        when(projectListDouble.getProject(1)).thenReturn(projectDouble);
+        when(projectDouble.getProductBacklog()).thenReturn(productBacklogDouble);
+        when(productBacklogDouble.createAndAddUserStory(userStoryDTODouble, 0)).thenReturn(false);
+        // create a new (real) controller
+        CreateUserStoryController controller = new CreateUserStoryController(projectListDouble);
+
+        // Act
+        boolean result = controller.createUserStory(1, userStoryDTODouble, 0);
+
+        // Assert
+        assertFalse(result);
+    }
+
+    @DisplayName("create and add user story from null DTO throws exception")
+    @Test
+    void createUserStoryWithNullDTOThrowsException() {
+        // Arrange
+        // create all necessary mock objects
+        UserStoryDTO dto = null;
+
+        ProjectList projectListDouble = mock(ProjectList.class);
+
+        CreateUserStoryController controller = new CreateUserStoryController(projectListDouble);
+
+        String expectedMessage = "UserStoryDTO must not be null";
+
+        // Act
+        IllegalArgumentException result = assertThrows(IllegalArgumentException.class, () -> {
+            controller.createUserStory(1, dto, 0);
+        });
+        String resultMessage = result.getMessage();
+
+        // Assert
+        assertEquals(expectedMessage, resultMessage);
+    }
+}
