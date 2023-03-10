3a2b5548d84e8a9cf7135145752b1cab6a73c8bb
Deborah Oliveira [1221830] <1221830@isep.ipp.pt>
2023-03-10 23:57:32 +0000

    Refactoring unit tests: mockito implementation in the resourceListTest.

--- src/test/java/org/switch2022/project/model/ResourceListTest.java
+++ src/test/java/org/switch2022/project/model/ResourceListTest.java
@@ -2,145 +2,163 @@ package org.switch2022.project.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class ResourceListTest {
 
     @Test
-    @DisplayName("ensure the resource has been added.")
-    void ensureAddResourceTrue() {
-        //arrange
-        Project project = new Project(1, "test", "test");
-        ResourceDTO resourceDTO = new ResourceDTO("deborah@hotmail.com", "Team Member", 1, new Date(), 25, 100);
-        Account account = new Account("Deborah", "deborah@hotmail.com", "938966782", new Profile("User"));
-        Role role = new Role("Team Member");
+    @DisplayName("Ensure the resource has been added.")
+    void addResourceIsSuccessful() {
+        //Arrange
+        Project project = mock(Project.class);
+        ResourceDTO resourceDTO = mock(ResourceDTO.class);
+        Account account = mock(Account.class);
+        Role role = mock(Role.class);
+        Mockito.when(role.getName()).thenReturn("Team Member");
         ResourceList resourceList = new ResourceList();
 
-        //act
+        //Act
         boolean result = resourceList.addResource(account, role, project, resourceDTO);
 
-        //assert
+        //Assert
         assertTrue(result);
     }
 
     @Test
-    @DisplayName("ensure the same resource cannot been added twice")
-    void ensureAddResourceFalse() {
-        //arrange
-        Project project = new Project(1, "test", "test");
-        ResourceDTO resourceDTO = new ResourceDTO("deborah@hotmail.com", "Team Member", 1, new Date(), 25, 100);
-        Account account = new Account("Deborah", "deborah@hotmail.com", "938966782", new Profile("User"));
-        Role role = new Role("Team Member");
+    @DisplayName("Ensure the same resource cannot been added twice")
+    void addSameResourceFails() {
+        //Arrange
+        Project project = mock(Project.class);
+        ResourceDTO resourceDTO = mock(ResourceDTO.class);
+        Account account = mock(Account.class);
+        Role role = mock(Role.class);
+        Mockito.when(role.getName()).thenReturn("Team Member");
         ResourceList resourceList = new ResourceList();
 
-        //act
-        boolean added = resourceList.addResource(account, role, project, resourceDTO);
-        boolean result = resourceList.addResource(account,role,project,resourceDTO);
+        //Act
+        resourceList.addResource(account, role, project, resourceDTO);
+        boolean result = resourceList.addResource(account, role, project, resourceDTO);
 
-        //assert
-        assertTrue(added);
+        //Assert
         assertFalse(result);
     }
 
     @Test
-    @DisplayName("ensure more than one team member can be added")
+    @DisplayName("Ensure more than one team member can be added")
     void addTwoTeamMembersIsSuccessful() {
-        //arrange
-        Project project = new Project(1, "test", "test");
-        ResourceDTO resourceDTO = new ResourceDTO("deborah@hotmail.com", "Team Member", 1, new Date(), 25, 100);
-        ResourceDTO resourceDTO1 = new ResourceDTO("pedro@example.com", "Team Member", 1, new Date(),50,95);
-        Account account = new Account("Deborah", "deborah@hotmail.com", "938966782", new Profile("User"));
-        Account account1 = new Account("Pedro", "pedro@example.com", "9666666", new Profile("User"));
-        Role role = new Role("Team Member");
+        //Arrange
+        Project project = mock(Project.class);
+        ResourceDTO firstResourceDTO = mock(ResourceDTO.class);
+        ResourceDTO secondResourceDTO = mock(ResourceDTO.class);
+        Account firstAccount = mock(Account.class);
+        Account secondAccount = mock(Account.class);
+        Role role = mock(Role.class);
+        Mockito.when(role.getName()).thenReturn("Team Member");
         ResourceList resourceList = new ResourceList();
-        //act
-        boolean result = resourceList.addResource(account, role, project, resourceDTO);
-        boolean result1 = resourceList.addResource(account1, role, project, resourceDTO1);
-        // assert
+
+        //Act
+        resourceList.addResource(firstAccount, role, project, firstResourceDTO);
+        boolean result = resourceList.addResource(secondAccount, role, project, secondResourceDTO);
+
+        //Assert
         assertTrue(result);
-        assertTrue(result1);
     }
 
     @Test
-    @DisplayName("ensure a PO can be successfully added")
+    @DisplayName("Ensure a PO can be successfully added")
     void addPOSuccess() {
-        // arrange
-        Project project = new Project(1, "test", "test");
-        ResourceDTO resourceDTO = new ResourceDTO("deborah@hotmail.com", "Team Member", 1, new Date(), 25, 100);
-        Account account = new Account("Deborah", "deborah@hotmail.com", "938966782", new Profile("User"));
-        Role role = new Role("Product Owner");
+        //Arrange
+        Project project = mock(Project.class);
+        ResourceDTO resourceDTO = mock(ResourceDTO.class);
+        Account account = mock(Account.class);
+        Role role = mock(Role.class);
+        Mockito.when(role.getName()).thenReturn("Product Owner");
         ResourceList resourceList = new ResourceList();
-        // act
+
+        //Act
         boolean result = resourceList.addResource(account, role, project, resourceDTO);
-        // assert
+
+        //Assert
         assertTrue(result);
     }
 
     @Test
-    @DisplayName("ensure a second PO cannot be added")
+    @DisplayName("Ensure a second PO cannot be added")
     void addSecondPOFails() {
-        //arrange
-        Project project = new Project(1, "test", "test");
-        ResourceDTO resourceDTO = new ResourceDTO("deborah@hotmail.com", "Team Member", 1, new Date(), 25, 100);
-        ResourceDTO resourceDTO1 = new ResourceDTO("pedro@example.com","Team Member",1,new Date(),50,95);
-        Account account = new Account("Deborah", "deborah@hotmail.com", "938966782", new Profile("User"));
-        Account account1 = new Account("Pedro", "pedro@example.com", "966666", new Profile("User"));
-        Role role = new Role("Product Owner");
+        //Arrange
+        Project project = mock(Project.class);
+        ResourceDTO firstResourceDTO = mock(ResourceDTO.class);
+        ResourceDTO secondResourceDTO = mock(ResourceDTO.class);
+        Account firstAccount = mock(Account.class);
+        Account secondAccount = mock(Account.class);
+        Role role = mock(Role.class);
+        Mockito.when(role.getName()).thenReturn("Product Owner");
         ResourceList resourceList = new ResourceList();
-        // act
-        boolean result = resourceList.addResource(account, role, project, resourceDTO);
-        boolean result1 = resourceList.addResource(account1, role, project, resourceDTO1);
-        // assert
-        assertTrue(result);
-        assertFalse(result1);
+
+        //Act
+        resourceList.addResource(firstAccount, role, project, firstResourceDTO);
+        boolean result = resourceList.addResource(secondAccount, role, project, secondResourceDTO);
+
+        //Assert
+        assertFalse(result);
     }
 
     @Test
-    @DisplayName("ensure a second SM cannot be added")
+    @DisplayName("Ensure a second SM cannot be added")
     void addSecondSMFails() {
-        //arrange
-        Project project = new Project(1, "test", "test");
-        ResourceDTO resourceDTO = new ResourceDTO("deborah@hotmail.com", "Team Member", 1, new Date(), 25, 100);
-        ResourceDTO resourceDTO1 = new ResourceDTO("pedro@example.com","Team Member",1,new Date(),50,95);
-        Account account = new Account("Deborah", "deborah@hotmail.com", "938966782", new Profile("User"));
-        Account account1 = new Account("Pedro", "pedro@example.com", "966666", new Profile("User"));
-        Role role = new Role("Scrum Master");
+        //Arrange
+        Project project = mock(Project.class);
+        ResourceDTO firstResourceDTO = mock(ResourceDTO.class);
+        ResourceDTO secondResourceDTO = mock(ResourceDTO.class);
+        Account firstAccount = mock(Account.class);
+        Account secondAccount = mock(Account.class);
+        Role role = mock(Role.class);
+        Mockito.when(role.getName()).thenReturn("Scrum Master");
         ResourceList resourceList = new ResourceList();
-        // act
-        boolean result = resourceList.addResource(account, role, project, resourceDTO);
-        boolean result1 = resourceList.addResource(account1, role, project, resourceDTO1);
-        // assert
-        assertTrue(result);
-        assertFalse(result1);
+
+        //Act
+        resourceList.addResource(firstAccount, role, project, firstResourceDTO);
+        boolean result = resourceList.addResource(secondAccount, role, project, secondResourceDTO);
+
+        //Assert
+        assertFalse(result);
     }
 
     @Test
     @DisplayName("get a list of resources as a list of DTOs")
-        void getAListOfResources() {
-        // arrange
-        Project project = new Project(1, "test", "test");
-        Account account = new Account("Deborah", "deborah@hotmail.com", "938966782", new Profile("User"));
-        Account account1 = new Account("Pedro", "pedro@example.com", "9666666", new Profile("User"));
-        Role role = new Role("Team Member");
-        ResourceDTO resourceDTO = new ResourceDTO("deborah@hotmail.com", "Team Member", 1, new Date(), 25, 100);
-        ResourceDTO resourceDTO1 = new ResourceDTO("pedro@example.com", "Team Member", 1, new Date(),50,95);
+    void getAListOfResources() {
+        //Arrange
+        Project project = mock(Project.class);
+        Account firstAccount = mock(Account.class);
+        Mockito.when(firstAccount.getEmail()).thenReturn("deborah@hotmail.com");
+        Account secondAccount = mock(Account.class);
+        Mockito.when(secondAccount.getEmail()).thenReturn("pedro@example.com");
+        Role role = mock(Role.class);
+        Mockito.when(role.getName()).thenReturn("Team Member");
+
+        ResourceDTO resourceDTO = new ResourceDTO("deborah@hotmail.com", "Team Member", 1, new Date(10-03-2023), 25, 100);
+        ResourceDTO resourceDTO1 = new ResourceDTO("pedro@example.com", "Team Member", 1, new Date(10-03-2021), 50, 95);
         ResourceList resourceList = new ResourceList();
-        resourceList.addResource(account, role, project, resourceDTO);
-        resourceList.addResource(account1, role, project, resourceDTO1);
+        resourceList.addResource(firstAccount, role, project, resourceDTO);
+        resourceList.addResource(secondAccount, role, project, resourceDTO1);
 
         List<ResourceDTO> listExpected = new ArrayList<>();
         ResourceDTO resourceDTO_Output = new ResourceDTO("deborah@hotmail.com", "Team Member");
         ResourceDTO resourceDTO1_Output = new ResourceDTO("pedro@example.com", "Team Member");
         listExpected.add(resourceDTO_Output);
         listExpected.add(resourceDTO1_Output);
-        // act
-        List<ResourceDTO> listResult =  resourceList.listResources();
-        // assert
+
+        //Act
+        List<ResourceDTO> listResult = resourceList.listResources();
+
+        //Assert
         assertEquals(listExpected, listResult);
     }
 
