5d11577834ecb22ffe97e2f536fa70f3492c21ca
Deborah Oliveira [1221830] <1221830@isep.ipp.pt>
2023-03-12 18:04:17 +0000

    Refactoring unit tests: mockito implementation in the ListResourceControllerTest.

--- src/test/java/org/switch2022/project/controller/ListResourcesControllerTest.java
+++ src/test/java/org/switch2022/project/controller/ListResourcesControllerTest.java
@@ -2,6 +2,7 @@ package org.switch2022.project.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.switch2022.project.model.*;
 
 import java.util.ArrayList;
@@ -9,37 +10,35 @@ import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class ListResourcesControllerTest {
     @Test
     @DisplayName("get a list of resources as a list of DTOs")
-    void getAListOfResources() {
-        // arrange
-        // --Setup for project:
-        Project project = new Project(1, "test", "test");
-        Account account = new Account("Deborah", "deborah@hotmail.com", "938966782", new Profile("User"));
-        Account account1 = new Account("Pedro", "pedro@example.com", "9666666", new Profile("User"));
-        Role role = new Role("Team Member");
-        ResourceDTO resourceDTO = new ResourceDTO("deborah@hotmail.com", "Team Member", 1, new Date(), 25, 100);
-        ResourceDTO resourceDTO1 = new ResourceDTO("pedro@example.com", "Team Member", 1, new Date(),50,95);
-        project.addResource(account, role, resourceDTO);
-        project.addResource(account1, role, resourceDTO1);
-        // --Add project to a projectList:
-        ProjectList projectList = new ProjectList();
-        projectList.addProject(project);
-        // -- Set projectlist as an attribute of ListResourcesController
+    void GetAListOfResources() {
+        //Arrange
+        Project project = mock(Project.class);
+        List<ResourceDTO> resources = new ArrayList<>();
+
+        resources.add(new ResourceDTO("deborah@hotmail.com", "Team Member"));
+        resources.add(new ResourceDTO("pedro@example.com", "Team Member"));
+
+        Mockito.when(project.listResources()).thenReturn(resources);
+
+        ProjectList projectList = mock(ProjectList.class);
+        Mockito.when(projectList.getProject(1)).thenReturn(project);
+
         ListResourcesController listResourcesController = new ListResourcesController(projectList);
 
-        // --Setup for the expected output:
+        //Create expected result
         List<ResourceDTO> listExpected = new ArrayList<>();
-        ResourceDTO resourceDTO_Output = new ResourceDTO("deborah@hotmail.com", "Team Member");
-        ResourceDTO resourceDTO1_Output = new ResourceDTO("pedro@example.com", "Team Member");
-        listExpected.add(resourceDTO_Output);
-        listExpected.add(resourceDTO1_Output);
-        // act
+        listExpected.add(new ResourceDTO("deborah@hotmail.com", "Team Member"));
+        listExpected.add (new ResourceDTO("pedro@example.com", "Team Member"));
+
+        //Act
         List<ResourceDTO> listResult = listResourcesController.listResources(1);
-        // assert
+
+        //Assert
         assertEquals(listExpected, listResult);
     }
-
 }
