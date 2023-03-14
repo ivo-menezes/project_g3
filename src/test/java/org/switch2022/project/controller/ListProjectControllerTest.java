b59737e68be1a6195c9ae057938de9691ffd5c09
Marisa Graça [1221849] <1221849@isep.ipp.pt>
2023-03-14 16:41:58 +0000

    Refactoring unit tests: mockito implementation in ListProjectControllerTest and ListUserProjectsControllerTest.

--- src/test/java/org/switch2022/project/controller/ListProjectControllerTest.java
+++ src/test/java/org/switch2022/project/controller/ListProjectControllerTest.java
@@ -2,15 +2,17 @@ package org.switch2022.project.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.switch2022.project.model.*;
+import org.switch2022.project.model.Project;
+import org.switch2022.project.model.ProjectDTO;
+import org.switch2022.project.model.ProjectList;
 
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class ListProjectControllerTest {
 
@@ -35,36 +37,48 @@ public class ListProjectControllerTest {
     }
 
     @Test
-    @DisplayName("Test whether the Controller can create the Project DTO list")
-    void testForCreatingProjectDTOList1() {
-
-        Project project = new Project(200, "proj1", new Date(2023, Calendar.JANUARY, 10), new Date(2024, Calendar.JANUARY, 22), "client1", "Planned");
-        Project projectTwo = new Project(201, "proj2", new Date(2003, Calendar.JANUARY, 10), new Date(2004, Calendar.JANUARY, 20), "client2", "Planned");
-        Project projectThree = new Project(202, "proj3", new Date(2010, Calendar.FEBRUARY, 24), new Date(2026, Calendar.FEBRUARY, 24), "client3", "Planned");
-
-        ProjectList projectList = new ProjectList();
-        projectList.addProject(project);
-        projectList.addProject(projectTwo);
-        projectList.addProject(projectThree);
-
+    @DisplayName("ensure that a valid list of project DTO is created")
+    void ensureThatAValidListOfProjectDTOisCreated() {
+        // arrange
+        // create all necessary mock objects
+        Project project1 = mock(Project.class);
+        Project project2 = mock(Project.class);
+        Project project3 = mock(Project.class);
+        ProjectList projectList = mock(ProjectList.class);
+        ProjectDTO projectDTO1 = mock(ProjectDTO.class);
+        ProjectDTO projectDTO2 = mock(ProjectDTO.class);
+        ProjectDTO projectDTO3 = mock(ProjectDTO.class);
+        // train the mock objects
+        when(projectList.listSize()).thenReturn(3);
+        when(projectList.getProjectByIndex(0)).thenReturn(project1);
+        when(projectList.getProjectByIndex(1)).thenReturn(project2);
+        when(projectList.getProjectByIndex(2)).thenReturn(project3);
+        when(projectList.createProjectDTO(project1)).thenReturn(projectDTO1);
+        when(projectList.createProjectDTO(project2)).thenReturn(projectDTO2);
+        when(projectList.createProjectDTO(project3)).thenReturn(projectDTO3);
+        // create a new (real) controller
         ListProjectController controller = new ListProjectController(projectList);
-        List result = controller.listProject(projectList);
-        List<Integer> notExpected = Arrays.asList(200, 201, 202, 203);
 
-        assertNotEquals(notExpected, result);
+        // act
+        List<ProjectDTO> result = controller.listProject(projectList);
+
+        //assert
+        assertTrue(result.size() == 3);
     }
 
     @Test
-    void checkTheEmptyList(){
-        ProjectList projectList = new ProjectList();
-        AtomicReference<ListProjectController> controller = null;
-        IllegalArgumentException result = assertThrows(IllegalArgumentException.class,
-                () -> controller.set(new ListProjectController(projectList)));
+    @DisplayName("ensure an empty project list throws an exception")
+    public void ensureEmptyProjectListThrowsException_WithMocks(){
+        // arrange
+        ProjectList projectList = mock(ProjectList.class);
+
+        IllegalArgumentException result = assertThrows(IllegalArgumentException.class, () -> new ListProjectController(projectList));
         String resultMessage = result.getMessage();
 
         // assert
         assertEquals("List is empty", resultMessage);
     }
+
     @Test
     public void testListProject_ShouldReturnListOProjectDTO() {
 
--- src/test/java/org/switch2022/project/controller/ListUserProjectsControllerTest.java
+++ src/test/java/org/switch2022/project/controller/ListUserProjectsControllerTest.java
@@ -1,163 +1,85 @@
 package org.switch2022.project.controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.switch2022.project.model.*;
 
-import java.util.*;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ListUserProjectsControllerTest {
 
-    /***
-     * Test if the Controller can create the UserProjectsDTO list.
-     */
     @Test
-    public void createListUserProjectsDTO() {
-        Project project1 = new Project(1,"Test1","For testing purposes");
-        Project project2 = new Project(2,"Test2","For testing purposes");
+    @DisplayName("ensure that a valid list of user projects is returned")
+    public void ensureValidListOfUserProjectsIsReturned() {
+        // arrange
+        // create all necessary mock objects
+        Account currentAccount = mock(Account.class);
+        ProjectList projectList = mock(ProjectList.class);
+        Project project1 = mock(Project.class);
+        Project project2 = mock(Project.class);
+        ResourceList resourceList1 = mock(ResourceList.class);
+        ResourceList resourceList2 = mock(ResourceList.class);
 
-        Profile profile = new Profile("User");
+        Resource resourceA1 = mock(Resource.class);
+        Resource resourceA2 = mock(Resource.class);
+        Resource resourceB1 = mock(Resource.class);
+        Resource resourceB2 = mock(Resource.class);
 
-        Role role1 = new Role( "Team Member");
-        Role role2 = new Role( "Scrum Master");
-        Role role3 = new Role( "Project Owner");
+        UserProjectsDTO userProjectsDTO_B1 = mock(UserProjectsDTO.class);
 
-        Account account1 = new Account ("Ricardo", "11111x@gmail.com", "33399988", profile);
-        Account account2 = new Account ("Filipe", "22222x@gmail.com", "33399987", profile);
-        Account account3 = new Account ("Tavares", "33333x@gmail.com", "33399986", profile);
+        // train the mock objects
+        when(projectList.listSize()).thenReturn(2);
+        when(currentAccount.getEmail()).thenReturn("xxxxx@gmail.com");
+        when(projectList.getProjectByIndex(0)).thenReturn(project1);
+        when(projectList.getProjectByIndex(1)).thenReturn(project2);
+        when(project1.getList()).thenReturn(resourceList1);
+        when(project2.getList()).thenReturn(resourceList2);
+        when(project1.getList().listResourceSize()).thenReturn(2);
+        when(project2.getList().listResourceSize()).thenReturn(2);
+        when(project1.getList().getResourceIndex(0)).thenReturn(resourceA1);
+        when(project1.getList().getResourceIndex(1)).thenReturn(resourceA2);
+        when(project2.getList().getResourceIndex(0)).thenReturn(resourceB1);
+        when(project2.getList().getResourceIndex(1)).thenReturn(resourceB2);
+        when(resourceA1.getEmailOfResource()).thenReturn("yyyyy@gmail.com");
+        when(resourceA2.getEmailOfResource()).thenReturn("zzzzz@gmail.com");
+        when(resourceB1.getEmailOfResource()).thenReturn("xxxxx@gmail.com");
+        when(resourceB2.getEmailOfResource()).thenReturn("yyyyy@gmail.com");
+        when(resourceA1.isActive()).thenReturn(true);
+        when(resourceA2.isActive()).thenReturn(true);
+        when(resourceB1.isActive()).thenReturn(true);
+        when(resourceB2.isActive()).thenReturn(true);
+        when(project2.createUserProjectsDTO(project2)).thenReturn(userProjectsDTO_B1);
+        when(project2.getCode()).thenReturn(2);
 
-        ResourceDTO resourceDTO1 = new ResourceDTO ("11111x@gmail.com", "DDD", 1, new Date(2023, Calendar.JANUARY, 1), 50, 80);
-        ResourceDTO resourceDTO2 = new ResourceDTO ("11111x@gmail.com", "SXXX",2, new Date(2023, Calendar.JANUARY, 2), 40, 50);
-        ResourceDTO resourceDTO3 = new ResourceDTO ("22222x@gmail.com", "xssc", 1, new Date(2023, Calendar.JANUARY, 3), 40, 80);
-        ResourceDTO resourceDTO4 = new ResourceDTO ("33333x@gmail.com", "dfgfd", 1, new Date(2023, Calendar.JANUARY,4), 40, 60);
-        ResourceDTO resourceDTO5 = new ResourceDTO ("22222x@gmail.com", "devjv", 2, new Date(2023, Calendar.JANUARY, 5), 40, 80);
-        ResourceDTO resourceDTO6 = new ResourceDTO ("33333x@gmail.com", "dkjr", 2, new Date(2023, Calendar.JANUARY, 5), 40, 80);
-
-        ProjectList projectList = new ProjectList();
-        projectList.addProject(project1);
-        projectList.addProject(project2);
-
-        project1.addResource(account1,role1,resourceDTO1);
-        project2.addResource(account1,role1,resourceDTO2);
-        project1.addResource(account2,role2,resourceDTO3);
-        project1.addResource(account3,role3,resourceDTO4);
-        project2.addResource(account2,role3,resourceDTO5);
-        project2.addResource(account3,role2,resourceDTO6);
-
-
+        // create a new (real) controller
         ListUserProjectsController controller = new ListUserProjectsController(projectList);
 
-        controller.listUserProjects(account1);
+        // act
+        List<UserProjectsDTO> result = controller.listUserProjects(currentAccount);
 
-        assertNotNull(controller);
+        assertTrue(result.size() == 1);
     }
 
-    /***
-     * Entering an empty list will give a message of error.
-     */
     @Test
-    public void checkTheEmptyList(){
-        ProjectList projectList = new ProjectList();
-        AtomicReference<ListUserProjectsController> controller = null;
-        IllegalArgumentException result = assertThrows(IllegalArgumentException.class,
-                () -> controller.set(new ListUserProjectsController(projectList)));
+    @DisplayName("ensure an empty project list throws an exception")
+    public void ensureEmptyProjectListThrowsException(){
+        // arrange
+        ProjectList projectList = mock(ProjectList.class);
+
+        IllegalArgumentException result = assertThrows(IllegalArgumentException.class, () -> new ListUserProjectsController(projectList));
         String resultMessage = result.getMessage();
 
         // assert
         assertEquals("List is empty", resultMessage);
     }
 
-    /***
-     * This test will ensure the resulting list doesn't come out as empty as well.
-     */
-    @Test
-    public void listUserProjectsNotEmpty() {
-        Project project1 = new Project(1,"Test1","For testing purposes");
-        Project project2 = new Project(2,"Test2","For testing purposes");
-
-        Profile profile = new Profile("User");
-
-        Role role1 = new Role( "Team Member");
-        Role role2 = new Role( "Scrum Master");
-        Role role3 = new Role( "Project Owner");
-
-        Account account1 = new Account ("Ricardo", "11111x@gmail.com", "33399988", profile);
-        Account account2 = new Account ("Filipe", "22222x@gmail.com", "33399987", profile);
-        Account account3 = new Account ("Tavares", "33333x@gmail.com", "33399986", profile);
-
-        ResourceDTO resourceDTO1 = new ResourceDTO ("11111x@gmail.com", "DDD", 1, new Date(2023, Calendar.JANUARY, 1), 50, 80);
-        ResourceDTO resourceDTO2 = new ResourceDTO ("11111x@gmail.com", "SXXX",2, new Date(2023, Calendar.JANUARY, 2), 40, 50);
-        ResourceDTO resourceDTO3 = new ResourceDTO ("22222x@gmail.com", "xssc", 1, new Date(2023, Calendar.JANUARY, 3), 40, 80);
-        ResourceDTO resourceDTO4 = new ResourceDTO ("33333x@gmail.com", "dfgfd", 1, new Date(2023, Calendar.JANUARY,4), 40, 60);
-        ResourceDTO resourceDTO5 = new ResourceDTO ("22222x@gmail.com", "devjv", 2, new Date(2023, Calendar.JANUARY, 5), 40, 80);
-        ResourceDTO resourceDTO6 = new ResourceDTO ("33333x@gmail.com", "dkjr", 2, new Date(2023, Calendar.JANUARY, 5), 40, 80);
-
-        ProjectList projectList = new ProjectList();
-        projectList.addProject(project1);
-        projectList.addProject(project2);
-
-        project1.addResource(account1,role1,resourceDTO1);
-        project2.addResource(account1,role1,resourceDTO2);
-        project1.addResource(account2,role2,resourceDTO3);
-        project1.addResource(account3,role3,resourceDTO4);
-        project2.addResource(account2,role3,resourceDTO5);
-        project2.addResource(account3,role2,resourceDTO6);
-
-
-        ListUserProjectsController controller = new ListUserProjectsController(projectList);
-        List result = controller.listUserProjects(account1);
-        assertFalse(result.isEmpty());
-    }
-
-    /***
-     * The first assertTrue of the method below will check if the result is an instance of the List class
-     * and not another type of objet.
-     * The second assertTrue checks if the first element of the returned list is an instance of the UserProjectsDTO class.
-     */
-    @Test
-    public void returnListOfUserProjectsDTO() {
-        Project project1 = new Project(1,"Test1","For testing purposes");
-        Project project2 = new Project(2,"Test2","For testing purposes");
-        Project project3 = new Project(3, "Test3", "For testing purposes");
-
-        Profile profile = new Profile("User");
-
-        Role role1 = new Role( "Team Member");
-        Role role2 = new Role( "Scrum Master");
-        Role role3 = new Role( "Project Owner");
-
-        Account account1 = new Account ("Ricardo", "11111x@gmail.com", "33399988", profile);
-        Account account2 = new Account ("Filipe", "22222x@gmail.com", "33399987", profile);
-        Account account3 = new Account ("Tavares", "33333x@gmail.com", "33399986", profile);
-
-        ResourceDTO resourceDTO1 = new ResourceDTO ("11111x@gmail.com", "DDD", 1, new Date(2023, Calendar.JANUARY, 1), 50, 80);
-        ResourceDTO resourceDTO2 = new ResourceDTO ("11111x@gmail.com", "SXXX",2, new Date(2023, Calendar.JANUARY, 2), 40, 50);
-        ResourceDTO resourceDTO3 = new ResourceDTO ("22222x@gmail.com", "xssc", 1, new Date(2023, Calendar.JANUARY, 3), 40, 80);
-        ResourceDTO resourceDTO4 = new ResourceDTO ("33333x@gmail.com", "dfgfd", 1, new Date(2023, Calendar.JANUARY,4), 40, 60);
-        ResourceDTO resourceDTO5 = new ResourceDTO ("22222x@gmail.com", "devjv", 2, new Date(2023, Calendar.JANUARY, 5), 40, 80);
-        ResourceDTO resourceDTO6 = new ResourceDTO ("33333x@gmail.com", "dkjr", 2, new Date(2023, Calendar.JANUARY, 5), 40, 80);
-        ResourceDTO resourceDTO7 = new ResourceDTO ("33333x@gmail.com", "fkkme",3,new Date(2023, Calendar.JANUARY, 6), 30, 80);
-
-        ProjectList projectList = new ProjectList();
-        projectList.addProject(project1);
-        projectList.addProject(project2);
-        projectList.addProject(project3);
-
-        project1.addResource(account1,role1,resourceDTO1);
-        project2.addResource(account1,role1,resourceDTO2);
-        project1.addResource(account2,role2,resourceDTO3);
-        project1.addResource(account3,role3,resourceDTO4);
-        project2.addResource(account2,role3,resourceDTO5);
-        project2.addResource(account3,role2,resourceDTO6);
-        project3.addResource(account3,role3,resourceDTO7);
-
-        ListUserProjectsController controller = new ListUserProjectsController(projectList);
-        List<UserProjectsDTO> result = controller.listUserProjects(account1);
-        assertTrue(result instanceof List);
-        assertTrue(result.get(0) instanceof UserProjectsDTO);
-    }
 
     /***
      * This test will compare two userProjectsLists will compare 2 lists (one created by the userProjectsListController
@@ -194,43 +116,51 @@ class ListUserProjectsControllerTest {
         assertNotEquals(expected,notExpected);
     }
 
-    /***
-     * This test will compare two userProjectsLists will compare 2 lists (one created by the userProjectsListController
-     * (this one is empty) and another by us) and conclude that they are not equal.
-     */
     @Test
-    public void listUserProjectsEmpty() {
-        Project project1 = new Project(1,"Test1","For testing purposes");
-        Project project2 = new Project(2,"Test2","For testing purposes");
+    @DisplayName("ensure that an empty list of user projects is returned")
+    public void ensureThatEmptyListOfUserProjectsIsReturned() {
+        // arrange
+        // create all necessary mock objects
+        Account currentAccount = mock(Account.class);
+        ProjectList projectList = mock(ProjectList.class);
+        Project project1 = mock(Project.class);
+        Project project2 = mock(Project.class);
+        ResourceList resourceList1 = mock(ResourceList.class);
+        ResourceList resourceList2 = mock(ResourceList.class);
 
-        Profile profile = new Profile("User");
+        Resource resourceA1 = mock(Resource.class);
+        Resource resourceA2 = mock(Resource.class);
+        Resource resourceB1 = mock(Resource.class);
+        Resource resourceB2 = mock(Resource.class);
 
-        Role role2 = new Role( "Scrum Master");
-        Role role3 = new Role( "Project Owner");
+        // train the mock objects
+        when(projectList.listSize()).thenReturn(2);
+        when(currentAccount.getEmail()).thenReturn("xxxxx@gmail.com");
+        when(projectList.getProjectByIndex(0)).thenReturn(project1);
+        when(projectList.getProjectByIndex(1)).thenReturn(project2);
+        when(project1.getList()).thenReturn(resourceList1);
+        when(project2.getList()).thenReturn(resourceList2);
+        when(project1.getList().listResourceSize()).thenReturn(2);
+        when(project2.getList().listResourceSize()).thenReturn(2);
+        when(project1.getList().getResourceIndex(0)).thenReturn(resourceA1);
+        when(project1.getList().getResourceIndex(1)).thenReturn(resourceA2);
+        when(project2.getList().getResourceIndex(0)).thenReturn(resourceB1);
+        when(project2.getList().getResourceIndex(1)).thenReturn(resourceB2);
+        when(resourceA1.getEmailOfResource()).thenReturn("yyyyy@gmail.com");
+        when(resourceA2.getEmailOfResource()).thenReturn("zzzzz@gmail.com");
+        when(resourceB1.getEmailOfResource()).thenReturn("zzzzz@gmail.com");
+        when(resourceA2.getEmailOfResource()).thenReturn("yyyyy@gmail.com");
+        when(resourceA1.isActive()).thenReturn(true);
+        when(resourceA2.isActive()).thenReturn(true);
+        when(resourceB1.isActive()).thenReturn(true);
+        when(resourceB2.isActive()).thenReturn(true);
 
-        Account account1 = new Account ("Ricardo", "11111x@gmail.com", "33399988", profile);
-        Account account2 = new Account ("Filipe", "22222x@gmail.com", "33399987", profile);
-        Account account3 = new Account ("Tavares", "33333x@gmail.com", "33399986", profile);
-
-        ResourceDTO resourceDTO3 = new ResourceDTO ("22222x@gmail.com", "xssc", 1, new Date(2023, Calendar.JANUARY, 3), 40, 80);
-        ResourceDTO resourceDTO4 = new ResourceDTO ("33333x@gmail.com", "dfgfd", 1, new Date(2023, Calendar.JANUARY,4), 40, 60);
-        ResourceDTO resourceDTO5 = new ResourceDTO ("22222x@gmail.com", "devjv", 2, new Date(2023, Calendar.JANUARY, 5), 40, 80);
-        ResourceDTO resourceDTO6 = new ResourceDTO ("33333x@gmail.com", "dkjr", 2, new Date(2023, Calendar.JANUARY, 5), 40, 80);
-
-        ProjectList projectList = new ProjectList();
-        projectList.addProject(project1);
-        projectList.addProject(project2);
-
-        project1.addResource(account2,role2,resourceDTO3);
-        project1.addResource(account3,role3,resourceDTO4);
-        project2.addResource(account2,role3,resourceDTO5);
-        project2.addResource(account3,role2,resourceDTO6);
-
+        // create a new (real) controller
         ListUserProjectsController controller = new ListUserProjectsController(projectList);
-        List result = controller.listUserProjects(account1);
-        List<Integer> notExpected = Arrays.asList(1);
 
-        assertNotSame(notExpected,result);
-        assertTrue(result.isEmpty());
+        // act
+        List<UserProjectsDTO> result = controller.listUserProjects(currentAccount);
+
+        assertTrue(result.size() == 0);
     }
 }
