bdbf91f92edcfc379416a2f879ebb0e9b313164d
Ivo Menezes [1221838] <1221838@isep.ipp.pt>
2023-03-15 16:49:50 +0000

    US019:Implementation:CreateSprintController(New Tests)

--- src/test/java/org/switch2022/project/controller/CreateSprintControllerTest.java
+++ src/test/java/org/switch2022/project/controller/CreateSprintControllerTest.java
@@ -1,5 +1,6 @@
 package org.switch2022.project.controller;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.switch2022.project.model.Project;
@@ -9,6 +10,7 @@ import org.switch2022.project.model.SprintDTO;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -33,5 +35,60 @@ class CreateSprintControllerTest {
         boolean result = controller.createSprint(projectCode,sprintDTO);
 
         assertTrue(result);
+    }
+    @Test
+    @DisplayName("Check if the provided projectList is null")
+    void checkTheProjectListIsNull(){
+        ProjectList projectListMock = null;
+
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            CreateSprintController controller = new CreateSprintController(projectListMock);
+        });
+        Assertions.assertEquals("ProjectList must not be null.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Check if the provided DTO is null")
+    void checkIfDTOIsNull(){
+        int projectCode = 1;
+        SprintDTO sprintDTOMock = null;
+        Project projectMock = mock(Project.class);
+        ProjectList projectListMock = mock(ProjectList.class);
+        when(projectListMock.getProject(projectCode)).thenReturn(projectMock);
+        CreateSprintController controller = new CreateSprintController(projectListMock);
+
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            controller.createSprint(projectCode, sprintDTOMock);
+        });
+        Assertions.assertEquals("SprintDTO must not be null.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Check if the second DTO is not inserted.")
+    void checkIfTheMethodComesFalse() throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+
+        int projectCode = 1;
+        Project projectMock = mock(Project.class);
+        ProjectList projectListMock = mock(ProjectList.class);
+        when(projectListMock.getProject(projectCode)).thenReturn(projectMock);
+        SprintDTO sprintDTO = new SprintDTO();
+        sprintDTO.sprintNumber = 1;
+        sprintDTO.startDate = formatter.parse("01/02/2022");
+        sprintDTO.endDate = formatter.parse("15/02/2022");
+
+        SprintDTO sprintDTOTwo = new SprintDTO();
+        sprintDTO.sprintNumber = 1;
+        sprintDTO.startDate = formatter.parse("01/02/2022");
+        sprintDTO.endDate = formatter.parse("15/02/2022");
+
+        when(projectMock.addSprint(sprintDTO)).thenReturn(true);
+        when(projectMock.addSprint(sprintDTOTwo)).thenReturn(false);
+
+        CreateSprintController controller = new CreateSprintController(projectListMock);
+        controller.createSprint(projectCode,sprintDTO);
+        boolean result = controller.createSprint(projectCode,sprintDTOTwo);
+
+        assertFalse(result);
     }
 }
