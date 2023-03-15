e16f67e098b52ec40e2a19ebdee7a43b7d5d5bc3
Ricardo Tavares [1221857] <1221857@isep.ipp.pt>
2023-03-15 22:56:14 +0000

    US021 : Implementation : implement EstimateEffortController + unit tests

---
+++ src/main/java/org/switch2022/project/controller/EstimateEffortController.java
@@ -0,0 +1,22 @@
+package org.switch2022.project.controller;
+
+import org.switch2022.project.model.*;
+
+public class EstimateEffortController {
+    private final ProjectList projectList;
+
+    public EstimateEffortController (ProjectList projectList) {
+        if (projectList == null) {
+            throw new IllegalArgumentException("Project List must not be null");
+        }
+        this.projectList = projectList;
+    }
+
+    public boolean estimateEffortUserStory (int projectCode, int sprintNumber, String userStoryID, double effort) {
+        Project project = projectList.getProject(projectCode);
+        SprintList sprintList = project.getSprintList();
+        Sprint sprint = sprintList.getSprint(sprintNumber);
+
+        return sprint.estimateEffortForUserStory(userStoryID, effort);
+    }
+}
---
+++ src/test/java/org/switch2022/project/controller/EstimateEffortControllerTest.java
@@ -0,0 +1,94 @@
+package org.switch2022.project.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.switch2022.project.model.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class EstimateEffortControllerTest {
+
+    @DisplayName("estimating a controller with null project list throws exception")
+    @Test
+    void estimateControllerWithNullProjectListThrowsException() {
+        // Arrange
+        String expectedMessage = "Project List must not be null";
+        ProjectList projectList = null;
+        // Act
+        IllegalArgumentException result = assertThrows(IllegalArgumentException.class, () -> {
+            new EstimateEffortController(projectList);
+        });
+        String resultMessage = result.getMessage();
+        // Assert
+        assertEquals(expectedMessage, resultMessage);
+    }
+
+
+    @DisplayName("estimating the effort of an user story succeeds")
+    @Test
+    void estimateEffortUserStorySucceeds(){
+        // Arrange
+        ProjectList projectListDouble = mock(ProjectList.class);
+        Project projectDouble = mock(Project.class);
+        SprintList sprintListDouble = mock(SprintList.class);
+        Sprint sprintDouble = mock(Sprint.class);
+        UserStory userStoryDouble1 = mock(UserStory.class);
+        UserStory userStoryDouble2 = mock(UserStory.class);
+
+        when(projectListDouble.getProject(1)).thenReturn(projectDouble);
+        when(projectDouble.getSprintList()). thenReturn(sprintListDouble);
+        when(sprintListDouble.getSprint(1)). thenReturn(sprintDouble);
+        when(userStoryDouble1.getId()).thenReturn("US001");
+        when(userStoryDouble2.getId()).thenReturn("US002");
+        when(sprintDouble.estimateEffortForUserStory("US002", 3.0)).thenReturn(true);
+
+        projectListDouble.addProject(projectDouble);
+        sprintListDouble.add(sprintDouble);
+        sprintDouble.addUserStoryToSprintBacklog(userStoryDouble1);
+        sprintDouble.addUserStoryToSprintBacklog(userStoryDouble2);
+
+        // create a new (real) controller
+        EstimateEffortController controller = new EstimateEffortController(projectListDouble);
+
+        // Act
+        boolean result = controller.estimateEffortUserStory(1, 1, "US002", 3.0);
+
+        // Assert
+        assertTrue(result);
+    }
+
+    @DisplayName("estimating the effort of an user story fails")
+    @Test
+    void estimateEfforUserStoryFails() {
+        // Arrange
+        ProjectList projectListDouble = mock(ProjectList.class);
+        Project projectDouble = mock(Project.class);
+        SprintList sprintListDouble = mock(SprintList.class);
+        Sprint sprintDouble = mock(Sprint.class);
+        UserStory userStoryDouble1 = mock(UserStory.class);
+        UserStory userStoryDouble2 = mock(UserStory.class);
+
+        when(projectListDouble.getProject(1)).thenReturn(projectDouble);
+        when(projectDouble.getSprintList()). thenReturn(sprintListDouble);
+        when(sprintListDouble.getSprint(1)). thenReturn(sprintDouble);
+        when(userStoryDouble1.getId()).thenReturn("US001");
+        when(userStoryDouble2.getId()).thenReturn("US002");
+        when(sprintDouble.estimateEffortForUserStory("US003", 3.0)).thenReturn(false);
+
+        projectListDouble.addProject(projectDouble);
+        sprintListDouble.add(sprintDouble);
+        sprintDouble.addUserStoryToSprintBacklog(userStoryDouble1);
+        sprintDouble.addUserStoryToSprintBacklog(userStoryDouble2);
+
+        // create a new (real) controller
+        EstimateEffortController controller = new EstimateEffortController(projectListDouble);
+
+        // Act
+        boolean result = controller.estimateEffortUserStory(1, 1, "US003", 3.0);
+
+        // Assert
+        assertFalse(result);
+    }
+}
