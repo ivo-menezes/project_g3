a8470628d073fa377654b439106c528d1090971b
Ricardo Tavares [1221857] <1221857@isep.ipp.pt>
2023-04-11 16:09:35 +0100

    US017: Refactor: Deleted duplicate FactoryUserStoryImpl.java from model package (resolved #7)

--- src/main/java/org/switch2022/project/model/FactoryUserStoryImpl.java
+++
@@ -1,8 +0,0 @@
-package org.switch2022.project.model;
-
-public class FactoryUserStoryImpl implements IFactoryUserStory{
-    @Override
-    public UserStory createUserStory(String id, String actor, String text, String acceptanceCriteria) {
-        return new UserStory(id, actor, text, acceptanceCriteria);
-    }
-}
