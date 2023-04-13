a0b059d39559edf8302cfbce512620ddce0c2505
Tiago Silva [1221867] <1221867@isep.ipp.pt>
2023-04-13 14:14:42 +0100

    US017: DDD Implementation: Create UserStoryActor & UserStoryActorTest (Close #29)

---
+++ src/main/java/org/switch2022/project/model/valueobject/UserStoryActor.java
@@ -0,0 +1,34 @@
+package org.switch2022.project.model.valueobject;
+
+import org.switch2022.project.ddd.ValueObject;
+
+public class UserStoryActor implements ValueObject {
+
+    private String userStoryActor;
+
+    public UserStoryActor (String userStoryActor) {
+        if (userStoryActor == null || userStoryActor.isBlank() || userStoryActor.isEmpty()) {
+            throw new IllegalArgumentException("User Story actor must not be empty");
+        }
+        this.userStoryActor = userStoryActor;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object)
+            return true;
+
+        if (object instanceof UserStoryActor) {
+            UserStoryActor userStoryActor1 = (UserStoryActor) object;
+
+            if (this.userStoryActor.equals(userStoryActor1.userStoryActor))
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return this.userStoryActor;
+    }
+}
---
+++ src/test/java/org/switch2022/project/model/valueobject/UserStoryActorTest.java
@@ -0,0 +1,106 @@
+package org.switch2022.project.model.valueobject;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserStoryActorTest {
+
+    final String expectedMessage = "User Story actor must not be empty";
+    final String userStoryActor1 = "Test1";
+    final String userStoryActor2 = "Test2";
+
+
+    @Test
+    public void shouldCreateAValidUserStoryActor () {
+        new UserStoryActor(userStoryActor1);
+    }
+
+    @Test
+    public void shouldThrowExceptionUserStoryActorWithNullUserStoryActor() {
+
+        Exception exception = assertThrows(Exception.class, () -> {
+            new UserStoryActor(null);
+        });
+
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void shouldThrowExceptionUserStoryActorWithBlankUserStoryActor() {
+
+        Exception exception = assertThrows(Exception.class, () -> {
+            new UserStoryActor("  ");
+        });
+
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void shouldThrowExceptionUserStoryActorWithEmptyUserStoryActor() {
+
+        Exception exception = assertThrows(Exception.class, () -> {
+            new UserStoryActor("");
+        });
+
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void shouldReturnFalseEqualsWithNull() {
+
+        UserStoryActor userStoryActor = new UserStoryActor(userStoryActor1);
+
+        boolean isEquals = userStoryActor.equals(null);
+
+        assertFalse(isEquals);
+    }
+
+    @Test
+    public void shouldReturnTrueEqualsWithSameObject() {
+
+        UserStoryActor userStoryActor = new UserStoryActor(userStoryActor1);
+
+        boolean isEquals = userStoryActor.equals(userStoryActor);
+
+        assertTrue(isEquals);
+    }
+
+    @Test
+    public void shouldReturnTrueEqualsWithSameUserStoryActor() {
+
+        UserStoryActor userStoryActor = new UserStoryActor(userStoryActor1);
+        UserStoryActor userStoryActorTest = new UserStoryActor(userStoryActor1);
+
+        boolean isEquals = userStoryActor.equals(userStoryActorTest);
+
+        assertTrue(isEquals);
+    }
+
+    @Test
+    public void shouldReturnFalseEqualsWithDifferentUserStoryActor() {
+
+        UserStoryActor userStoryActorTest1 = new UserStoryActor(userStoryActor1);
+        UserStoryActor userStoryActorTest2 = new UserStoryActor(userStoryActor2);
+
+        boolean isEquals = userStoryActorTest1.equals(userStoryActorTest2);
+
+        assertFalse(isEquals);
+    }
+
+    @Test
+    public void toStringShouldReturnTheUserStoryActorString() {
+
+        UserStoryActor userStoryActor = new UserStoryActor(userStoryActor1);
+
+        assertEquals(userStoryActor.toString(), userStoryActor1);
+    }
+
+
+}
