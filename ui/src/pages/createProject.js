48b7e35b510ef83fa5bf6d4e854eeb84efff0276
Tiago Silva [1221867] <1221867@isep.ipp.pt>
2023-04-30 16:17:03 +0100

    US023: UI : Implementation - createProject (Addresses #67)

--- ui/src/pages/createProject.js
+++ ui/src/pages/createProject.js
@@ -1,6 +1,9 @@
 import TextField from "../components/textField";
 import AppContext from "../context/AppContext";
-import {useContext} from "react";
+import React, {useContext} from "react";
+import Header from "../components/header";
+import Button from "../components/button";
+import {Link} from "react-router-dom";
 
 const CreateProject = () => {
     const {state, dispatch} = useContext(AppContext);
@@ -8,18 +11,82 @@ const CreateProject = () => {
     const {code} = textValue;
 
     return (
-        <section>
+        <div className="form-container">
+            <Header text="Create Project" />
             <form>
-                <h2> Fill in the information to create a project</h2>
                 <TextField
                     mandatory = {true}
                     label = 'Code Project'
                     placeholder = '...'
                     dispatch={dispatch}
                     value ={code}
+                    className="form-input"
                 />
+
+                <TextField
+                    mandatory = {true}
+                    label = 'Project Name'
+                    placeholder = '...'
+                    dispatch={dispatch}
+                    value ={code}
+                    className="form-input"
+                />
+
+                <TextField
+                    mandatory = {true}
+                    label = 'Project Description'
+                    placeholder = '...'
+                    dispatch={dispatch}
+                    value ={code}
+                    className="form-input"
+                />
+
+                <TextField
+                    mandatory = {true}
+                    label = 'Customer'
+                    placeholder = '...'
+                    dispatch={dispatch}
+                    value ={code}
+                    className="form-input"
+                />
+
+                <div>
+                    <label htmlFor="start-date" className="form-input label" >Start Date:</label>
+                    <input
+                        type="date"
+                        id="start-date"
+                        name="start-date"
+                        onChange={(event) => console.log(event.target.value)}
+                    />
+                </div>
+
+                <TextField
+                    mandatory = {true}
+                    label = 'Project Status'
+                    placeholder = 'PLANNED'
+                    dispatch={dispatch}
+                    value ={code}
+                    readOnly={true}
+                    className="form-input"
+                />
+
+                <TextField
+                    mandatory = {true}
+                    label = 'Budget'
+                    placeholder = '...'
+                    dispatch={dispatch}
+                    value ={code}
+                    className="form-input"
+                />
+
+                <div>
+                    <Button className="form-submit" name="Save" />
+                    <Link to="/listProjects">
+                        <Button className="form-submit" name="Cancel" />
+                    </Link>
+                </div>
             </form>
-        </section>
+        </div>
     );
 }
 export default CreateProject;
