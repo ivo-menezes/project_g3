41769bfd454ed56f5e37ef9d344036e613297bc9
Deborah Oliveira [1221830] <1221830@isep.ipp.pt>
2023-05-01 02:39:19 +0100

    US024: UI : Component table implementation in ListProjects page. (Addresses #61)

--- ui/src/pages/listProjects.js
+++ ui/src/pages/listProjects.js
@@ -1,13 +1,22 @@
+import React from "react";
+import Header from "../components/header";
 import Button from "../components/button";
-import Grid from "../components/grid";
-import { Link }  from "react-router-dom"
+import Table from "../components/table";
+import {Link} from "react-router-dom";
+import projects from "../store/projects";
+
+const headers = {
+    header1: {label: "Code"},
+    header2: {label: "Name"}
+}
 
 const ListProjects = () => {
     return (
         <div>
-            <Grid/>
+            <Header text='Project List' style={{marginLeft: "65px"}}/>
+            <Table data={projects} headers={headers}/>
             <Link to='/createProject'>
-            <Button className='button-ListProjects' name='Create Project'/>
+                <Button className='button-ListProjects' name='Create Project'/>
             </Link>
             <Link to='/testPage'>
                 <Button className='button-ListProjects' name='Test Page'/>
