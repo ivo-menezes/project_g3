2abfeba2afecae6b8fcbc8af6123cee8c81d58e2
Cristina Teixeira [1221827] <1221827@isep.ipp.pt>
2023-05-02 09:39:11 +0100

    US025: UI Implementation: change index.css and createUserStory to apply style. Change testPage. (closes #77)

--- ui/src/pages/createUserStory.js
+++ ui/src/pages/createUserStory.js
@@ -26,59 +26,54 @@ const CreateUserStory = () => {
     };
 
     return (
-        <div className="form-container">
-            <Header text="Create User Story" />
-            <form className="form" onSubmit={submitForm}>
-                <TextField
-                    label="Number:"
-                    value={number}
-                    placeholder=""
-                    mandatory={true}
-                    dispatch={dispatch}
-                    name="number"
-                    className="form-input"
+        <div className="form-create-userStory">
+            <Header className = "header-create-userStory" text="CREATE USER STORY" />
+            <form onSubmit={submitForm}>
+
+                <TextField className="textField"
+                           label="Number:"
+                           value={number}
+                           placeholder=""
+                           mandatory={true}
+                           dispatch={dispatch}
+
                 />
-                <TextField
-                    label="Actor:"
-                    value={actor}
-                    placeholder=""
-                    mandatory={true}
-                    dispatch={dispatch}
-                    name="actor"
-                    className="form-input"
+                <TextField className="textField"
+                           label="Actor:"
+                           value={actor}
+                           placeholder=""
+                           mandatory={true}
+                           dispatch={dispatch}
+
                 />
-                <TextField
-                    label="Description:"
-                    value={description}
-                    placeholder=""
-                    mandatory={true}
-                    dispatch={dispatch}
-                    name="description"
-                    className="form-input"
+                <TextField className="textField"
+                           label="Description:"
+                           value={description}
+                           placeholder=""
+                           mandatory={true}
+                           dispatch={dispatch}
+
                 />
-                <TextField
-                    label="Priority:"
-                    value={priority}
-                    placeholder="(optional)"
-                    mandatory={false}
-                    dispatch={dispatch}
-                    name="priority"
-                    className="form-input"
+                <TextField className="textField"
+                           label="Priority:"
+                           value={priority}
+                           placeholder="(optional)"
+                           mandatory={false}
+                           dispatch={dispatch}
+
                 />
-                <TextArea
-                    label="Acceptance Criteria:"
-                    value={ac}
-                    mandatory={true}
-                    placeholder=""
-                    dispatch={dispatch}
-                    className="form-input"
+                <TextArea className="textArea"
+                          label="Acceptance Criteria:"
+                          value={ac}
+                          mandatory={true}
+                          placeholder=""
+                          dispatch={dispatch}
                 />
-                <div>
-                    <Button className="form-submit" name="Save" onClick={submitForm} />
-                    <Link to="/testPage">
-                        <Button className="form-submit" name="Cancel" />
-                    </Link>
-                </div>
+
+                <Button className="button-form-createUserStory-save" name="Save" />
+                <Link to="/testPage">
+                    <Button className="button-form-createUserStory-cancel" name="Cancel" />
+                </Link>
             </form>
         </div>
     );
