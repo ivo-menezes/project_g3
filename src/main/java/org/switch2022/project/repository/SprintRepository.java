674d283a0de6e76a7a97cb7a5a40da3360fc08bc
[Joana Fonseca] <1221839@isep.ipp.pt>
2023-05-04 21:54:02 +0100

    US019: Implementation: Created SprintRepository and tests. (resolved #85)

---
+++ src/main/java/org/switch2022/project/repository/SprintRepository.java
@@ -0,0 +1,65 @@
+package org.switch2022.project.repository;
+
+import org.switch2022.project.ddd.Repository;
+import org.switch2022.project.model.sprint.SprintDDD;
+import org.switch2022.project.model.valueobject.SprintID;
+
+import java.util.*;
+
+@org.springframework.stereotype.Repository
+public class SprintRepository implements Repository<SprintID, SprintDDD> {
+
+    /***
+     * This creates the necessary hashmap, called sprintData, to link SprintID and Sprint.
+     */
+    private static final Map<SprintID, SprintDDD> sprintData = new HashMap<>();
+
+    /***
+     * This will check whether the HashMap already has a sprint with the id we pass
+     * into this method.
+     * @param id As SprintID object
+     * @return boolean value of whether it contains the ID or not
+     */
+    public boolean containsID(SprintID id) {
+        return sprintData.containsKey(id);
+    }
+
+    /***
+     * This method will save the sprint to the Hashmap if there is none containing the
+     * sprintID we have
+     * @param sprint Object to save into the Repository
+     * @return true if process is successful, false if the process fails.
+     */
+    public boolean save(SprintDDD sprint) {
+        SprintID sprintID = sprint.identity();
+
+        if(!containsID(sprintID)){
+            sprintData.put(sprintID, sprint);
+            return true;
+        }
+        return false;
+    }
+
+    /***
+     * This method returns the collection of values of the hashmap,
+     * by using the values() method, as an iterable object
+     * @return iterable collection of Sprint values in the hashmap
+     */
+    public Iterable<SprintDDD> findAll() {
+        return sprintData.values();
+    }
+
+    /***
+     * This method will return a sprint if there is one in the hashmap
+     * with the SprintID passed by the user. Otherwise, it will return optional
+     * @param id of the SprintID Object Type
+     * @return sprint or optional
+     */
+    public Optional<SprintDDD> getByID(SprintID id) {
+        if (!containsID(id)) {
+            return Optional.empty();
+        } else {
+            return Optional.of(sprintData.get(id));
+        }
+    }
+}
---
+++ src/test/java/org/switch2022/project/repository/SprintRepositoryTest.java
@@ -0,0 +1,154 @@
+package org.switch2022.project.repository;
+
+import org.junit.jupiter.api.Test;
+import org.switch2022.project.model.sprint.SprintDDD;
+import org.switch2022.project.model.valueobject.ProjectCode;
+import org.switch2022.project.model.valueobject.SprintID;
+import org.switch2022.project.model.valueobject.SprintNumber;
+import org.switch2022.project.model.valueobject.TimePeriod;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class SprintRepositoryTest {
+
+
+    @Test
+    public void checkIfRepositoryIsCorrectlyCalled(){
+        new SprintRepository();
+    }
+
+    @Test
+    public void checkIfRepositoryAddsSprintCorrectly(){
+        SprintRepository sprintData = new SprintRepository();
+        SprintDDD sprintMock = mock(SprintDDD.class);
+
+        boolean result = sprintData.save(sprintMock);
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void checkIfRepositoryDoesNotContainID(){
+        SprintRepository sprintData = new SprintRepository();
+        SprintID sprintIDMock = mock(SprintID.class);
+
+        boolean result = sprintData.containsID(sprintIDMock);
+
+        assertFalse(result);
+    }
+    private int countIterable(Iterable<?> iterable) {
+        int count = 0;
+        for (Object item : iterable) {
+            count++;
+        }
+        return count;
+    }
+    @Test
+    public void checkIfRepositoryHasAnythingInIt(){
+        SprintRepository sprintDataNew = new SprintRepository();
+
+        Iterable<SprintDDD> result = sprintDataNew.findAll();
+
+        assertEquals(0, countIterable(result));
+    }
+    @Test
+    public void checkIfRepositoryIsNotEmpty() throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        SprintRepository sprintData = new SprintRepository();
+
+        TimePeriod newTimePeriod = new TimePeriod(formatter.parse("01/02/2022"), formatter.parse("15/02/2022"));
+        TimePeriod anotherTimePeriod = new TimePeriod(formatter.parse("16/02/2022"), formatter.parse("25/02/2022"));
+        ProjectCode projectCode = new ProjectCode("PT6");
+        SprintNumber newSprintNumber = new SprintNumber(5);
+        SprintNumber anotherSprintNumber = new SprintNumber(6);
+
+        SprintID sprintID = new SprintID(projectCode, newSprintNumber);
+        SprintID sprintIDNumberTwo = new SprintID(projectCode, anotherSprintNumber);
+
+        SprintDDD newSprint = new SprintDDD(sprintID, newTimePeriod);
+        SprintDDD anotherSprint = new SprintDDD(sprintIDNumberTwo, anotherTimePeriod);
+
+        sprintData.save(newSprint);
+        sprintData.save(anotherSprint);
+
+        Iterable<SprintDDD> result = sprintData.findAll();
+
+        assertEquals(3, countIterable(result));
+    }
+     @Test
+    public void checkIfRepositoryContainsID() throws ParseException {
+         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+         SprintRepository sprintData = new SprintRepository();
+
+         TimePeriod newTimePeriod = new TimePeriod(formatter.parse("01/02/2022"), formatter.parse("15/02/2022"));
+         ProjectCode projectCode = new ProjectCode("PT6");
+         SprintNumber newSprintNumber = new SprintNumber(5);
+
+         SprintID sprintID = new SprintID(projectCode, newSprintNumber);
+         SprintID sprintIDNumberTwo = new SprintID(projectCode, newSprintNumber);
+
+         SprintDDD newSprint = new SprintDDD(sprintID, newTimePeriod);
+
+         sprintData.save(newSprint);
+         boolean result = sprintData.containsID(sprintIDNumberTwo);
+
+         assertFalse(result);
+     }
+
+    @Test
+    public void checkIfRepositoryHasSprintWithID() throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        SprintRepository sprintData = new SprintRepository();
+
+        TimePeriod newTimePeriod = new TimePeriod(formatter.parse("01/02/2022"), formatter.parse("15/02/2022"));
+        TimePeriod anotherTimePeriod = new TimePeriod(formatter.parse("16/02/2022"), formatter.parse("25/02/2022"));
+        ProjectCode projectCode = new ProjectCode("PT6");
+        SprintNumber newSprintNumber = new SprintNumber(5);
+        SprintNumber anotherSprintNumber = new SprintNumber(6);
+
+        SprintID sprintID = new SprintID(projectCode, newSprintNumber);
+        SprintID sprintIDNumberTwo = new SprintID(projectCode, anotherSprintNumber);
+
+        SprintDDD newSprint = new SprintDDD(sprintID, newTimePeriod);
+        SprintDDD anotherSprint = new SprintDDD(sprintIDNumberTwo, anotherTimePeriod);
+
+        sprintData.save(newSprint);
+        sprintData.save(anotherSprint);
+
+        Optional result = sprintData.getByID(sprintIDNumberTwo);
+
+        assertTrue(result.isPresent());
+    }
+
+    @Test
+    public void checkIfRepositoryObtainsSprintWithID() throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        SprintRepository sprintData = new SprintRepository();
+
+        TimePeriod newTimePeriod = new TimePeriod(formatter.parse("01/02/2022"), formatter.parse("15/02/2022"));
+        TimePeriod anotherTimePeriod = new TimePeriod(formatter.parse("16/02/2022"), formatter.parse("25/02/2022"));
+        ProjectCode projectCode = new ProjectCode("PT6");
+        SprintNumber newSprintNumber = new SprintNumber(5);
+        SprintNumber anotherSprintNumber = new SprintNumber(6);
+        SprintNumber thirdSprintNumber = new SprintNumber(7);
+
+        SprintID sprintID = new SprintID(projectCode, newSprintNumber);
+        SprintID sprintIDNumberTwo = new SprintID(projectCode, anotherSprintNumber);
+        SprintID sprintIDNumberThree = new SprintID(projectCode, thirdSprintNumber);
+
+        SprintDDD newSprint = new SprintDDD(sprintID, newTimePeriod);
+        SprintDDD anotherSprint = new SprintDDD(sprintIDNumberTwo, anotherTimePeriod);
+
+        sprintData.save(newSprint);
+        sprintData.save(anotherSprint);
+
+        Optional result = sprintData.getByID(sprintIDNumberThree);
+
+        assertTrue(result.isEmpty());
+    }
+}
