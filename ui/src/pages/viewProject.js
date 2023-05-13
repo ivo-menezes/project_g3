c3639e86fd91fc3bcc6b86a658890875a79aa6ca
Ivo Menezes [1221838] <1221838@isep.ipp.pt>
2023-05-13 23:37:15 +0100

    UI: Implementation: viewProject.js and projects.js (Update)(Addresses #93)

--- ui/src/pages/viewProject.js
+++ ui/src/pages/viewProject.js
@@ -24,10 +24,19 @@ const ViewProject = () => {
                     <ul>
                         <li className="project-info">NAME: {project.title} </li>
                         <li className="project-info">CODE: {project.id} </li>
+                        <li className="project-info">DESCRIPTION: {project.description} </li>
+                        <li className="project-info">CUSTOMER: {project.customer} </li>
+                        <li className="project-info">START DATE: {project.startDate} </li>
+                        <li className="project-info">END DATE: {project.endDate} </li>
+                        <li className="project-info">STATUS: {project.status} </li>
+                        <li className="project-info">TYPOLOGY: {project.typology} </li>
+                        <li className="project-info">SPRINT DURATION: {project.sprintDuration} </li>
+                        <li className="project-info">NUMBER OF PLANNED SPRINTS: {project.numberOfPlannedSprints} </li>
+                        <li className="project-info">BUDGET: {project.budget} </li>
                     </ul>
                 </>
             ) : (
-                <p>No project found with code</p>
+                <p>No project found with code {id}</p>
             )}
             <div className="bt-container ">
                 <Link to={`/listSprints/${id}`}>
--- ui/src/store/projects.js
+++ ui/src/store/projects.js
@@ -2,51 +2,141 @@ const projects = [
     {
         id: 1,
         title: 'Project Switch',
+        description: 'Project management tool: A software that enables teams to collaborate, plan, and manage their projects and tasks',
+        customer: 'NovaTech Solutions',
+        startDate: '1/10/2022',
+        endDate: '31/06/2023',
+        status: "Planned",
+        typology: "Fixed Cost",
+        sprintDuration: '3 - Weeks',
+        numberOfPlannedSprints: 13,
+        budget: 3000
     },
 
     {
         id: 2,
-        title: 'Project B6'
+        title: 'Project B6',
+        description: 'Customer relationship management (CRM) system: A software that helps businesses manage interactions with customers, including sales, marketing, and customer support',
+        customer: 'BluePeak Industries',
+        startDate: '12/02/2022',
+        endDate: '12/11/2023',
+        status: "Planned",
+        typology: "Fixed Cost",
+        sprintDuration: '2 - Weeks',
+        numberOfPlannedSprints: 48,
+        budget: 100000
     },
 
     {
         id: 3,
-        title: 'Project Critical Tech Work'
+        title: 'Project Critical Tech Work',
+        description: 'Personal finance management application: A software that helps users track their income, expenses, and investments',
+        customer: 'StellarSoft',
+        startDate: '21/12/2022',
+        endDate: '12/11/2025',
+        status: "Planned",
+        typology: "Time and Materials",
+        sprintDuration: '2 - Weeks',
+        numberOfPlannedSprints: 78,
+        budget: 200000
     },
 
     {
         id: 4,
-        title: 'Project Natixis'
+        title: 'Project Natixis',
+        description:'Inventory management system: A software that helps businesses track and manage their inventory levels, orders, and sales',
+        customer: 'Horizon Ventures',
+        startDate: '12/02/2020',
+        endDate: '15/10/2023',
+        status: "Planned",
+        typology: "Fixed Cost",
+        sprintDuration: '2 - Weeks',
+        numberOfPlannedSprints: 71,
+        budget: 150000
     },
 
     {
         id: 5,
-        title: 'Project Present Technologies'
+        title: 'Project Present Technologies',
+        description: 'Content management system (CMS): A software that allows users to create, manage, and publish digital content, such as articles, videos, and images',
+        customer: 'OptimaTech',
+        startDate: '12/02/2022',
+        endDate: '31/11/2024',
+        status: "Planned",
+        typology: "Fixed Cost",
+        sprintDuration: '2 - Weeks',
+        numberOfPlannedSprints: 52,
+        budget: 50000
     },
 
     {
         id: 6,
-        title: 'Project Emergen'
+        title: 'Project Emergen',
+        description: 'Social networking application: A software that enables users to connect with each other, share content, and communicate online',
+        customer: 'QuantumSoft',
+        startDate: '10/02/2020',
+        endDate: '12/11/2022',
+        status: "Closed",
+        typology: "Fixed Cost",
+        sprintDuration: '2 - Weeks',
+        numberOfPlannedSprints: 49,
+        budget: 300000
     },
 
     {
         id: 7,
-        title: 'Project Blip'
+        title: 'Project Blip',
+        description: 'Game matchmaking service: A software that matches players with compatible opponents based on their skill level, location, and other criteria. It helps players find opponents quickly and efficiently and improves their overall gaming experience',
+        customer: 'Phoenix Solutions',
+        startDate: '16/10/2022',
+        endDate: '12/11/2024',
+        status: "Construction",
+        typology: "Fixed Cost",
+        sprintDuration: '3 - Weeks',
+        numberOfPlannedSprints: 34,
+        budget: 200000
     },
 
     {
         id: 8,
-        title: 'Project FMQ'
+        title: 'Project FMQ',
+        description: 'Multiplayer game server: A software that allows players to connect and play games with each other online. It manages game sessions, player authentication, and communication between players',
+        customer: 'Nexus Innovations',
+        startDate: '12/04/2023',
+        endDate: '12/11/2024',
+        status: "Elaboration",
+        typology: "Fixed Cost",
+        sprintDuration: '2 - Weeks',
+        numberOfPlannedSprints: 15,
+        budget: 50000
     },
 
     {
         id: 9,
-        title: 'Project Devexpert'
+        title: 'Project Devexpert',
+        description: 'Language learning application: A software that provides users with tools and resources to learn a new language',
+        customer: 'Summit Enterprises',
+        startDate: '14/02/2022',
+        endDate: '12/11/2023',
+        status: "Transition",
+        typology: "Fixed Cost",
+        sprintDuration: '2 - Weeks',
+        numberOfPlannedSprints: 20,
+        budget: 300000
     },
 
     {
         id: 10,
-        title: 'Critical Software'
+        title: 'Critical Software',
+        description: 'Electronic health record (EHR) system: A software that stores and manages patient health information for healthcare providers',
+        customer: 'FusionTech Inc',
+        startDate: '12/02/2021',
+        endDate: '12/11/2023',
+        status: "Construction",
+        typology: "Fixed Cost",
+        sprintDuration: '2 - Weeks',
+        numberOfPlannedSprints: 45,
+        budget: 350000
     },
 ]
 
