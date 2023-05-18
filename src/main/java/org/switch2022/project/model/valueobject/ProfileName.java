f553449a6bb854820af331c6ed667e5e1d135d1a
Tiago Silva [1221867] <1221867@isep.ipp.pt>
2023-05-18 16:48:08 +0100

    US028/US029/US030: Implementation: ProfileName + ProfileNameTest (Addresses #101)

---
+++ src/main/java/org/switch2022/project/model/valueobject/ProfileName.java
@@ -0,0 +1,29 @@
+package org.switch2022.project.model.valueobject;
+
+import org.switch2022.project.ddd.ValueObject;
+
+import java.util.Objects;
+
+public class ProfileName implements ValueObject {
+
+    private final String profileName;
+
+    public ProfileName(String profileName) {
+        if (profileName == null || profileName.isBlank() || profileName.isEmpty())
+            throw new IllegalArgumentException(("profileName cannot be null/blank/empty"));
+
+        this.profileName = profileName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ProfileName)) return false;
+        ProfileName that = (ProfileName) o;
+        return Objects.equals(profileName, that.profileName);
+    }
+
+    @Override
+    public int hashCode() { return Objects.hash(profileName);}
+
+}
---
+++ src/test/java/org/switch2022/project/model/valueobject/ProfileNameTest.java
@@ -0,0 +1,96 @@
+package org.switch2022.project.model.valueobject;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProfileNameTest {
+
+    @DisplayName("creating profileName with null value should throw Exception")
+    @Test
+    void createProfileNameWithNullValueThrowsException() {
+        // Arrange
+        String profileName = null;
+        String expectedMessage = "profileName cannot be null/blank/empty";
+
+        // Act
+        IllegalArgumentException result =assertThrows(IllegalArgumentException.class, () -> {
+            new ProfileName(profileName);
+        });
+
+        String resultMessage = result.getMessage();
+
+        // Assert
+        assertEquals(expectedMessage, resultMessage);
+    }
+
+    @DisplayName("creating profileName with empty value should throw Exception")
+    @Test
+    void createProfileNameWithEmptyValueThrowsException() {
+        // Arrange
+        String profileName = "";
+        String expectedMessage = "profileName cannot be null/blank/empty";
+
+        // Act
+        IllegalArgumentException result = assertThrows(IllegalArgumentException.class, () -> {
+            new ProfileName(profileName);
+        });
+        String resultMessage = result.getMessage();
+
+        // Assert
+        assertEquals(expectedMessage, resultMessage);
+    }
+
+    @DisplayName("creating profileName with blank value should throw Exception")
+    @Test
+    void createProfileNameWithWithBlankValueThrowsException() {
+        // Arrange
+        String profileName = "       ";
+        String expectedMessage = "profileName cannot be null/blank/empty";
+
+        // Act
+        IllegalArgumentException result = assertThrows(IllegalArgumentException.class, () -> {
+            new ProfileName(profileName);
+        });
+        String resultMessage = result.getMessage();
+
+        // Assert
+        assertEquals(expectedMessage, resultMessage);
+    }
+
+
+    @DisplayName("With same object we should return true")
+    @Test
+    void shouldReturnTrueEqualsSameObject() {
+        ProfileName profileName = new ProfileName("Test");
+
+        boolean isEquals = profileName.equals(profileName);
+
+        assertTrue(isEquals);
+    }
+
+    @DisplayName("With same profile name we should return true")
+    @Test
+    void shouldReturnTrueEqualsSameProfileName() {
+        ProfileName profileName1 = new ProfileName("Test");
+        ProfileName profileName2 = new ProfileName("Test");
+
+        boolean isEquals = profileName1.equals(profileName2);
+
+        assertTrue(isEquals);
+    }
+
+    @DisplayName("With differents profiles names we should return false")
+    @Test
+    void shouldReturnFalseEqualsDifferentProfileName() {
+        ProfileName profileName1 = new ProfileName("Test");
+        ProfileName profileName2 = new ProfileName("Test1");
+
+        boolean isEquals = profileName1.equals(profileName2);
+
+        assertFalse(isEquals);
+    }
+
+
+}
