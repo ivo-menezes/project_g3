5a15db395e8990e032b5026bad03fc22e2b05fa8
Tiago Silva [1221867] <1221867@isep.ipp.pt>
2023-05-18 16:49:29 +0100

    US028/US029/US030: Implementation: AccountStatus (Addresses #101)

---
+++ src/main/java/org/switch2022/project/model/valueobject/AccountStatus.java
@@ -0,0 +1,8 @@
+package org.switch2022.project.model.valueobject;
+
+import org.switch2022.project.ddd.ValueObject;
+
+public enum AccountStatus implements ValueObject {
+    Active,
+    Inactive
+}
