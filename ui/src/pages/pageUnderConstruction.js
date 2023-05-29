7d498dadcbe42c23c50d44e04fb8fd092f851527
Marisa Graça <1221849@isep.ipp.pt>
2023-05-29 23:35:53 +0100

    T608: Redesign UI layout: Updates on pageUnderConstruction and index.css  (addresses #125)

--- ui/src/pages/pageUnderConstruction.js
+++ ui/src/pages/pageUnderConstruction.js
@@ -1,11 +1,25 @@
 import React from "react";
+import Header from "../components/header";
 
 const PageUnderConstruction = () => {
     return (
-        <div className="string-format">
-            <h2>Page Under Construction</h2>
+        /*
+        <div className="welcome-page">
+            <div className="title-welcome-container">
+                <Header className="string-under-construction" text="Page under construction!"/>
+            </div>
         </div>
-    )
-}
+        */
+        <div className="welcome-page">
+            <div className="title-welcome-container">
+                <Header className="string-under-construction" text="We are RELENTLESS"/>
+                <div className="subtitle-welcome-container">
+                    <Header className="subtitle-welcome" text={<span>and working hard to build this page for you!
+                        <br/>Stay tunned</span>}/>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default PageUnderConstruction;
