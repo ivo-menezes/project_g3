09ee6dfe567e21165f11c7f7c0f22463aaeb4788
Tiago Silva [1221867] <1221867@isep.ipp.pt>
2023-05-30 15:14:10 +0100

    US012 : Implementation : Create ResourceID (Addresses #131)

---
+++ src/main/java/org/switch2022/project/model/valueobject/ResourceID.java
@@ -0,0 +1,15 @@
+package org.switch2022.project.model.valueobject;
+
+import org.switch2022.project.ddd.DomainId;
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class ResourceID implements DomainId, Serializable {
+
+    private Long id;
+
+    public ResourceID(Long id) {
+        this.id = id;
+    }
+}
