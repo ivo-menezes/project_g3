4a349b63ef3a504d25f5fbdcd9cfb5b31e5275f1
Ricardo Tavares [1221857] <1221857@isep.ipp.pt>
2023-05-31 10:19:48 +0100

    US028/US029/US030: Implementation: AccountIDTest updated (Addresses #115)

--- src/test/java/org/switch2022/project/model/valueobject/AccountIDTest.java
+++ src/test/java/org/switch2022/project/model/valueobject/AccountIDTest.java
@@ -3,63 +3,18 @@ package org.switch2022.project.model.valueobject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.UUID;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AccountIDTest {
-
+    
     @Test
-    @DisplayName("Ensure that objects with the same AccountID are equals.")
-    public void checkIfObjectsWithTheSameAccountIDAreEquals() {
-        AccountID accountID1 = new AccountID();
-        AccountID accountID2 = accountID1;
+    @DisplayName("Ensure that id is returned")
+    void ensureIDIsReturned() {
+        long expectedId = 1;
+        AccountID accountID = new AccountID(expectedId);
 
-        assertEquals(accountID1, accountID2);
+        long actualId = accountID.getId();
+        assertEquals(expectedId, actualId);
     }
 
-    @Test
-    @DisplayName("Ensure that objects with the same AccountID are equals.")
-    public void checkIfObjectsWithTheSameAccountIDAreNotEquals() {
-        AccountID accountID1 = new AccountID();
-        AccountID accountID2 = new AccountID();
-
-        assertNotEquals(accountID1, accountID2);
-    }
-
-    @Test
-    @DisplayName("Ensure that objects with the same AccountID have the same hash code.")
-    public void checkIfObjectsWithTheSameAccountIDHaveTheSameHashCode() {
-        AccountID accountID1 = new AccountID();
-        AccountID accountID2 = accountID1;
-
-        assertEquals(accountID1.hashCode(), accountID2.hashCode());
-    }
-
-    @Test
-    @DisplayName("Ensure that objects with the same AccountID have the same hash code.")
-    public void checkIfObjectsWithTheSameAccountIDHaveDifferentsHashCodes() {
-        AccountID accountID1 = new AccountID();
-        AccountID accountID2 = new AccountID();
-
-        assertNotEquals(accountID1.hashCode(), accountID2.hashCode());
-    }
-
-    @Test
-    @DisplayName("Ensure that equals returns false for different object types")
-    public void shouldReturnFalseForDifferentObjectTypes() {
-        AccountID accountID = new AccountID();
-        Object otherObject = new Object();
-
-        assertNotEquals(accountID,otherObject);
-    }
-
-    @Test
-    @DisplayName("Ensure that AccountID is returned and its not equal to another AccountID")
-    public void shouldReturnTheAccountID() {
-        AccountID accountID = new AccountID();
-        UUID uuid1 = UUID.randomUUID();
-
-        assertNotEquals(uuid1, accountID.getAccountID());
-    }
 }
