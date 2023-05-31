23686e3769f148d561afebe0dd4d3dca04e356a8
Ricardo Tavares [1221857] <1221857@isep.ipp.pt>
2023-05-31 10:05:42 +0100

    US028/US029/US030: Implementation: AccountID updated (Addresses #115)

--- src/main/java/org/switch2022/project/model/valueobject/AccountID.java
+++ src/main/java/org/switch2022/project/model/valueobject/AccountID.java
@@ -1,29 +1,15 @@
 package org.switch2022.project.model.valueobject;
 
 import org.switch2022.project.ddd.DomainId;
-import org.switch2022.project.ddd.ValueObject;
 
-import java.util.Objects;
-import java.util.UUID;
+import java.io.Serializable;
 
-public class AccountID implements DomainId {
+public class AccountID implements DomainId, Serializable {
 
-    final UUID accountID = UUID.randomUUID();
+    private long id;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof AccountID)) return false;
-        AccountID accountID1 = (AccountID) o;
-        return accountID.equals(accountID1.accountID);
+    public AccountID(long id) {
+        this.id = id;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(accountID);
-    }
-
-    public UUID getAccountID() {
-        return this.accountID;
-    }
 }
