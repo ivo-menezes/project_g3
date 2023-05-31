a4173287ac4ce78d3d0d363180ae2be2a708fbc0
Ricardo Tavares [1221857] <1221857@isep.ipp.pt>
2023-05-31 10:33:52 +0100

    US028/US029/US030: Implementation: CostPerHour updated (Addresses #105)

--- src/main/java/org/switch2022/project/model/valueobject/CostPerHour.java
+++ src/main/java/org/switch2022/project/model/valueobject/CostPerHour.java
@@ -2,9 +2,10 @@ package org.switch2022.project.model.valueobject;
 
 import org.switch2022.project.ddd.ValueObject;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class CostPerHour implements ValueObject {
+public class CostPerHour implements ValueObject, Serializable {
 
     private final double costPerHour;
 
