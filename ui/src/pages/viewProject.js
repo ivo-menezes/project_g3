2e1444c00ab6fbc00585dcc1e1962b759d92740d
Pedro Silva [1221855] <1221855@isep.ipp.pt>
2023-06-03 19:06:01 +0100

    US026: Implementation: changes to viewProject.js so project attributes are read correctly from context (addresses #160)

--- ui/src/pages/viewProject.js
+++ ui/src/pages/viewProject.js
@@ -17,17 +17,17 @@ const ViewProject = () => {
 
     // Define column labels and their corresponding attributes
     const columnLabels = [
-        { label: "Name:", attribute: "title" },
-        { label: "Code:", attribute: "id" },
+        { label: "Name:", attribute: "projectName" },
+        { label: "Code:", attribute: "projectCode" },
         { label: "Description:", attribute: "description" },
-        { label: "Customer:", attribute: "customer" },
+        { label: "Customer:", attribute: "customerID" },
         { label: "Start date:", attribute: "startDate" },
         { label: "End date:", attribute: "endDate" },
-        { label: "Status:", attribute: "status" },
-        { label: "Typology:", attribute: "typology" },
+        { label: "Status:", attribute: "projectStatus" },
+        { label: "Typology:", attribute: "typologyID" },
         { label: "Sprint duration:", attribute: "sprintDuration" },
-        { label: "Number of planned sprints:", attribute: "numberOfPlannedSprints" },
-        { label: "Budgets:", attribute: "budget" },
+        { label: "Number of planned sprints:", attribute: "projectNumberOfPlannedSprints" },
+        { label: "Budget:", attribute: "projectBudget" },
     ];
 
     return (
