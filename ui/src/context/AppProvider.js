e4196fb0fe315d7b8f16a1ad5b877d4df6bd54cf
Marisa Graça <1221849@isep.ipp.pt>
2023-06-03 12:31:03 +0100

    T628: UI: Prepare AppProvider.js to receive data from the API (closes #158)

--- ui/src/context/AppProvider.js
+++ ui/src/context/AppProvider.js
@@ -7,6 +7,9 @@ import backlogs from "../store/productBacklogs";
 import sprints from "../store/sprints";
 import resources from "../store/resources";
 
+/*
+Will delete this part when switching from the local store to the API.
+ */
 const AppProvider = (props) =>{
     const initialState = {
         projectList: projects,
@@ -18,7 +21,41 @@ const AppProvider = (props) =>{
         endDate: null,
     }
 
+    /* When switching from the local store to the API, all properties should be empty or null. Data will be
+    received from the H2 database in Runtime.
+
+    const AppProvider = (props) => {
+    const initialState = {
+    projectList: [],
+    backlogs: [],
+    sprints: [],
+    resources: [],
+    textValue: '',
+    startDate: null,
+    endDate: null,
+  };
+     */
+
     const [state, dispatch] = useReducer(reducer, initialState);
+
+    /* UseEffect can perform data-fetching side-effect.
+
+    useEffect(() => {
+    const fetchSetup = async () => {
+    try {
+      const response = await axios.get('/localhost:8080'); // API endpoint
+      const setup = response.data; // Assuming the response contains the setup data
+      dispatch({ type: 'ADD_SETUP', payload: setup }); // 'ADD_SETUP' needs to be created in Reducer.js.
+    } catch (error) {
+      console.error('Error fetching setup:', error);
+    }
+  };
+
+  fetchSetup();
+}, []);
+     */
+
+
     return (
         <Provider value={{
             state,
