23fee862e92357efb567c6c195a3a43be6d53cb1
[Joana Fonseca] <1221839@isep.ipp.pt>
2023-06-06 10:19:32 +0100

    Sprint cleaning: Implementation: Deleted unused classes. (addresses #166)

--- src/main/java/org/switch2022/project/mapper/AccountDTO.java
+++
@@ -1,9 +0,0 @@
-package org.switch2022.project.mapper;
-
-public class AccountDTO {
-    public String email;
-    public boolean status;
-
-    public AccountDTO(String email, boolean status) {
-    }
-}
--- src/main/java/org/switch2022/project/mapper/RegisterAccountDTO.java
+++
@@ -1,8 +0,0 @@
-package org.switch2022.project.mapper;
-
-public class RegisterAccountDTO {
-    public String name;
-    public String email;
-    public String phone;
-    public String photo;
-}
