cf1ef83555c700591c9f89eff77e7ae283d0381b
Ivo Menezes [1221838] <1221838@isep.ipp.pt>
2023-06-06 16:55:21 +0100

    T720: Implementation : changes to address Sonarqube metrics(Addresses #167)

--- src/main/java/org/switch2022/project/datamodel/JPA/SprintJPA.java
+++ src/main/java/org/switch2022/project/datamodel/JPA/SprintJPA.java
@@ -6,48 +6,55 @@ import java.util.Objects;
 
 @Entity
 @Table(name = "sprints")
-    public class SprintJPA {
+public class SprintJPA {
 
- @Id
- @AttributeOverrides({
-         @AttributeOverride(name = "projectCode", column = @Column(name = "ProjectCode")),
-         @AttributeOverride(name = "sprintNumber", column = @Column(name = "SprintNumber")),
- })
+    @Id
+    @AttributeOverrides({
+            @AttributeOverride(name = "projectCode", column = @Column(name = "ProjectCode")),
+            @AttributeOverride(name = "sprintNumber", column = @Column(name = "SprintNumber")),
+    })
     private SprintJpaID sprintID;
     @Temporal(TemporalType.DATE)
-     private Date startDate;
+    private Date startDate;
     @Temporal(TemporalType.DATE)
-      private Date endDate;
+    private Date endDate;
 
-     protected SprintJPA() {}
+    protected SprintJPA() {
+    }
 
     public SprintJPA(SprintJpaID sprintID, Date startDate, Date endDate) {
         this.sprintID = sprintID;
         this.startDate = startDate;
         this.endDate = endDate;
     }
+
     public SprintJpaID getSprintID() {
-         return sprintID;
-     }
+        return sprintID;
+    }
+
     public Date getStartDate() {
-         return startDate;
-     }
+        return startDate;
+    }
+
     public Date getEndDate() {
-         return endDate;
-     }
+        return endDate;
+    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) {
             return true;
         }
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         SprintJPA that = (SprintJPA) o;
         return Objects.equals(sprintID, that.sprintID) && Objects.equals(startDate, that.startDate) && Objects.equals(endDate, that.endDate);
     }
 
     @Override
     public int hashCode() {
+
         return Objects.hash(sprintID, startDate, endDate);
     }
 }
