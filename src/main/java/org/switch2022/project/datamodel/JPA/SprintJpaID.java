04172d041f56210892f4ca655d98bdaa368e4865
Ivo Menezes [1221838] <1221838@isep.ipp.pt>
2023-06-07 09:19:23 +0100

    T702: Implementation : changes to address Sonarqube metrics(Addresses #167)

--- src/main/java/org/switch2022/project/datamodel/JPA/SprintJpaID.java
+++ src/main/java/org/switch2022/project/datamodel/JPA/SprintJpaID.java
@@ -3,39 +3,48 @@ package org.switch2022.project.datamodel.JPA;
 import javax.persistence.Embeddable;
 import java.io.Serializable;
 import java.util.Objects;
+
 @Embeddable
-    public class SprintJpaID implements Serializable {
+public class SprintJpaID implements Serializable {
 
-        private String projectCode;
+    private String projectCode;
 
-        private int sprintNumber;
+    private int sprintNumber;
 
-        public SprintJpaID(String projectCode, int sprintNumber) {
-            this.projectCode = projectCode;
-            this.sprintNumber = sprintNumber;
-        }
+    public SprintJpaID(String projectCode, int sprintNumber) {
+        this.projectCode = projectCode;
+        this.sprintNumber = sprintNumber;
+    }
 
     protected SprintJpaID() {
 
     }
 
     public String getProjectCode() {
-            return projectCode;
-        }
 
-        public int getSprintNumber() { return sprintNumber; }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            SprintJpaID that = (SprintJpaID) o;
-            return Objects.equals(projectCode, that.projectCode) && Objects.equals(sprintNumber, that.sprintNumber);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(projectCode, sprintNumber);
-        }
+        return projectCode;
     }
+
+    public int getSprintNumber() {
+        return sprintNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        SprintJpaID that = (SprintJpaID) o;
+        return Objects.equals(projectCode, that.projectCode) && Objects.equals(sprintNumber, that.sprintNumber);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(projectCode, sprintNumber);
+    }
+}
 
