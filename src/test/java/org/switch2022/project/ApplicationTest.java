973c66db432ad220d991232e6bb09a02786acad5
Pedro Silva [1221855] <1221855@isep.ipp.pt>
2023-06-09 10:08:45 +0100

    T702: Implementation: add test to Application main method (addresses #167)

---
+++ src/test/java/org/switch2022/project/ApplicationTest.java
@@ -0,0 +1,14 @@
+package org.switch2022.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void shouldRunApplicationMainFunctionWithoutErrors() {
+        Application.main(new String[]{});
+    }
+
+}
