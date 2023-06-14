af1520e518cdfc256ce554d62e5f920d0d8e0976
Marisa Graça <1221849@isep.ipp.pt>
2023-06-14 12:38:49 +0100

    US036: Implementation: Update UserStoryInSprint and tests (close #184)

--- src/main/java/org/switch2022/project/model/sprint/UserStoryInSprint.java
+++ src/main/java/org/switch2022/project/model/sprint/UserStoryInSprint.java
@@ -3,33 +3,47 @@ package org.switch2022.project.model.sprint;
 import org.switch2022.project.ddd.DomainEntity;
 import org.switch2022.project.model.valueobject.UserStoryEffortEstimate;
 import org.switch2022.project.model.valueobject.UserStoryInSprintID;
+import org.switch2022.project.model.valueobject.UserStoryStatus;
+
+import java.util.Objects;
 
 /**
  * The class UserStoryInSprint represents user stories included in a specific sprint,
- * and associates a UserStoryInSprintID to a UserStoryEffortEstimate. These user stories
- * will be stored in a sprint backlog.
+ * and associates a UserStoryInSprintID to a UserStoryEffortEstimate and a UserStoryStatus
+ * (refers to the US status in a specific project and sprint, which may differ from
+ * the US status in the product backlog). These user stories will be stored in a
+ * sprint backlog.
  */
 public class UserStoryInSprint implements DomainEntity<UserStoryInSprintID> {
 
     private final UserStoryInSprintID userStoryInSprintID;
     private final UserStoryEffortEstimate userStoryEffortEstimate;
 
+    private final UserStoryStatus userStoryInSprintStatus;
+
     /**
      * Protected constructor to instantiate a UserStoryInSprint.
      * @param userStoryInSprintID encapsulates a UserStoryID and a SprintID;
-     * @param userStoryEffortEstimate encapsulates the effort associated to the UserStoryInSprint.
+     * @param userStoryEffortEstimate encapsulates the effort associated to the
+     *                                UserStoryInSprint;
+     * @param userStoryInSprintStatus encapsulates the status for UserStoryInSprint.
      */
     protected UserStoryInSprint(UserStoryInSprintID userStoryInSprintID,
-                                UserStoryEffortEstimate userStoryEffortEstimate) {
+                                UserStoryEffortEstimate userStoryEffortEstimate,
+                                UserStoryStatus userStoryInSprintStatus) {
         if (userStoryInSprintID == null) {
             throw new IllegalArgumentException("ID for User Story in Sprint cannot be null");
         }
         if (userStoryEffortEstimate == null) {
             throw new IllegalArgumentException("User Story effort estimate in Sprint cannot be null");
         }
+        if (userStoryInSprintStatus == null) {
+            throw new IllegalArgumentException("Status for User Story in Sprint cannot be null");
+        }
 
         this.userStoryInSprintID = userStoryInSprintID;
         this.userStoryEffortEstimate = userStoryEffortEstimate;
+        this.userStoryInSprintStatus = userStoryInSprintStatus;
     }
 
     /**
@@ -46,5 +60,30 @@ public class UserStoryInSprint implements DomainEntity<UserStoryInSprintID> {
      */
     protected UserStoryEffortEstimate getUserStoryEffortEstimate() {
         return userStoryEffortEstimate;
+    }
+
+    /**
+     * Retrieves the status associated to the UserStoryInSprint.
+     * @return userStoryInSprintStatus.
+     */
+    protected UserStoryStatus getUserStoryInSprintStatus() {
+        return userStoryInSprintStatus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        UserStoryInSprint that = (UserStoryInSprint) o;
+        return userStoryInSprintID.equals(that.userStoryInSprintID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userStoryInSprintID);
     }
 }
--- src/test/java/org/switch2022/project/model/sprint/UserStoryInSprintTest.java
+++ src/test/java/org/switch2022/project/model/sprint/UserStoryInSprintTest.java
@@ -1,26 +1,60 @@
 package org.switch2022.project.model.sprint;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.switch2022.project.model.valueobject.UserStoryEffortEstimate;
 import org.switch2022.project.model.valueobject.UserStoryInSprintID;
+import org.switch2022.project.model.valueobject.UserStoryStatus;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
 class UserStoryInSprintTest {
+
+    @Mock
+    private UserStoryInSprintID userStoryInSprintID;
+    @Mock
+    private UserStoryEffortEstimate userStoryEffortEstimate;
+    @Mock
+    private UserStoryStatus userStoryInSprintStatus;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    /**
+     * This test ensures that UserStoryInSprint is successfully created,
+     * using mocks for its attributes.
+     */
+    @Test
+    @DisplayName("ensure UserStoryInSprint is successfully created")
+    public void ensureUSInSprintCreated() {
+        // Act
+        UserStoryInSprint userStoryInSprint = new UserStoryInSprint(
+                userStoryInSprintID,
+                userStoryEffortEstimate,
+                userStoryInSprintStatus);
+
+        // Assert
+        assertInstanceOf(UserStoryInSprint.class, userStoryInSprint);
+    }
+
+    /**
+     * This test ensures that passing a null UserStoryInSprintID throwns an exception.
+     */
     @Test
     @DisplayName("ensure UserStoryInSprintID is not null")
     void ensureUSInSprintNotNull() {
         //Arrange
-        UserStoryEffortEstimate userStoryEffortEstimate = mock(UserStoryEffortEstimate.class);
         String expectedMessage = "ID for User Story in Sprint cannot be null";
 
         IllegalArgumentException exception = assertThrows(
-                IllegalArgumentException.class, () -> {
-                    new UserStoryInSprint(null, userStoryEffortEstimate);
-                }
+                IllegalArgumentException.class, () -> new UserStoryInSprint(null, userStoryEffortEstimate,
+                        userStoryInSprintStatus)
         );
         //Act
         String resultMessage = exception.getMessage();
@@ -28,34 +62,34 @@ class UserStoryInSprintTest {
         assertEquals(expectedMessage, resultMessage);
     }
 
+    /**
+     * This test ensures that a userStoryInSprintID is successfully returned.
+     */
     @Test
     @DisplayName("ensure userStoryInSprintID is returned")
     void ensureUSInSprintIDIsReturned() {
         //Arrange
-        UserStoryInSprintID userStoryInSprint_ID = mock(UserStoryInSprintID.class);
-        UserStoryEffortEstimate userStoryEffortEstimate = mock(UserStoryEffortEstimate.class);
-
-        UserStoryInSprint userStoryInSprint = new UserStoryInSprint(userStoryInSprint_ID, userStoryEffortEstimate);
-        UserStoryInSprintID expected = userStoryInSprint_ID;
+        UserStoryInSprint userStoryInSprint = new UserStoryInSprint(userStoryInSprintID,
+                userStoryEffortEstimate, userStoryInSprintStatus);
+        UserStoryInSprintID expected = userStoryInSprintID;
         //Act
         UserStoryInSprintID result = userStoryInSprint.identity();
         //Assert
         assertEquals(expected, result);
     }
 
-
-
+    /**
+     * This test ensures that passing a null UserStoryEffortEstimate throwns an exception.
+     */
     @Test
     @DisplayName("ensure UserStoryEffortEstimate is not null")
     void ensureUSEfforteEstimateNotNull() {
         //Arrange
-        UserStoryInSprintID userStoryInSprint_ID = mock(UserStoryInSprintID.class);
         String expectedMessage = "User Story effort estimate in Sprint cannot be null";
 
         IllegalArgumentException exception = assertThrows(
-                IllegalArgumentException.class, () -> {
-                    new UserStoryInSprint(userStoryInSprint_ID, null);
-                }
+                IllegalArgumentException.class, () -> new UserStoryInSprint(userStoryInSprintID, null,
+                        userStoryInSprintStatus)
         );
         //Act
         String resultMessage = exception.getMessage();
@@ -63,14 +97,15 @@ class UserStoryInSprintTest {
         assertEquals(expectedMessage, resultMessage);
     }
 
+    /**
+     * This test ensures that a UserStoryEffortEstimate is successfully returned.
+     */
     @Test
     @DisplayName("ensure UserStoryEffortEstimate is returned")
     void ensureUSEffortEstimateIsReturned() {
         //Arrange
-        UserStoryInSprintID userStoryInSprint_ID = mock(UserStoryInSprintID.class);
-        UserStoryEffortEstimate userStoryEffortEstimate = mock(UserStoryEffortEstimate.class);
-
-        UserStoryInSprint userStoryInSprint = new UserStoryInSprint(userStoryInSprint_ID, userStoryEffortEstimate);
+        UserStoryInSprint userStoryInSprint = new UserStoryInSprint(userStoryInSprintID,
+                userStoryEffortEstimate, userStoryInSprintStatus);
         UserStoryEffortEstimate expected = userStoryEffortEstimate;
         //Act
         UserStoryEffortEstimate result = userStoryInSprint.getUserStoryEffortEstimate();
@@ -78,4 +113,194 @@ class UserStoryInSprintTest {
         assertEquals(expected, result);
     }
 
+    /**
+     * This test ensures that passing a null UserStoryStatus throwns an exception.
+     */
+    @Test
+    @DisplayName("ensure UserStoryStatus is not null")
+    void ensureUSStatusNotNull() {
+        //Arrange
+        String expectedMessage = "Status for User Story in Sprint cannot be null";
+
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class, () -> new UserStoryInSprint(userStoryInSprintID, userStoryEffortEstimate,
+                        null)
+        );
+        //Act
+        String resultMessage = exception.getMessage();
+        //Assert
+        assertEquals(expectedMessage, resultMessage);
+    }
+
+    /**
+     * This test ensures that a UserStoryStatus is successfully returned.
+     */
+    @Test
+    @DisplayName("ensure UserStoryStatus is returned")
+    void ensureUSStatusIsReturned() {
+        //Arrange
+        UserStoryInSprint userStoryInSprint = new UserStoryInSprint(userStoryInSprintID,
+                userStoryEffortEstimate, userStoryInSprintStatus);
+        UserStoryStatus expected = userStoryInSprintStatus;
+        //Act
+        UserStoryStatus result = userStoryInSprint.getUserStoryInSprintStatus();
+        //Assert
+        assertEquals(expected, result);
+    }
+
+    /**
+     * This tests ensures that a created UserStoryInSprint is not null.
+     */
+    @Test
+    @DisplayName("ensure object does not equal null")
+    void ensureObjectNotEqualNull() {
+        //Arrange
+        UserStoryInSprint userStoryInSprint = new UserStoryInSprint(
+                userStoryInSprintID,
+                userStoryEffortEstimate,
+                userStoryInSprintStatus);
+        //Act
+        boolean result = userStoryInSprint.equals(null);
+        //Assert
+        assertFalse(result);
+    }
+
+    @Test
+    @DisplayName("ensure object equals itself")
+    void ensureObjectEqualsItself() {
+        //Arrange
+        UserStoryInSprint userStoryInSprint = new UserStoryInSprint(
+                userStoryInSprintID,
+                userStoryEffortEstimate,
+                userStoryInSprintStatus);
+
+        //Act
+        boolean result = userStoryInSprint.equals(userStoryInSprint);
+
+        //Assert
+        assertTrue(result);
+    }
+
+
+    /**
+     * This test ensures that 2 objects with the same userStoryInSprintID are equal.
+     */
+    @Test
+    @DisplayName("ensure object equals object with same userStoryInSprintID")
+    void objectEqualsSameUserStoryInSprintID() {
+        //Arrange
+        UserStoryInSprint userStoryInSprint1 = new UserStoryInSprint(
+                userStoryInSprintID,
+                userStoryEffortEstimate,
+                userStoryInSprintStatus);
+
+        UserStoryEffortEstimate userStoryEffortEstimate2 = mock(UserStoryEffortEstimate.class);
+        UserStoryStatus userStoryInSprintStatus2 = mock(UserStoryStatus.class);
+        UserStoryInSprint userStoryInSprint2 = new UserStoryInSprint(
+                userStoryInSprintID,
+                userStoryEffortEstimate2,
+                userStoryInSprintStatus2);
+
+        //Act
+        boolean result = userStoryInSprint1.equals(userStoryInSprint2);
+
+        //Assert
+        assertTrue(result);
+    }
+
+    /**
+     * test ensures that 2 objects with the different userStoryInSprintID are not equal.
+     */
+    @Test
+    @DisplayName("ensure object does not equal object with different userStoryInSprintID")
+    void objectDoesNotEqualUserStoryInSprintWithDifferentID() {
+        //Arrange
+        UserStoryInSprint userStoryInSprint1 = new UserStoryInSprint(
+                userStoryInSprintID,
+                userStoryEffortEstimate,
+                userStoryInSprintStatus);
+
+        UserStoryInSprintID userStoryInSprintID2 = mock(UserStoryInSprintID.class);
+        UserStoryInSprint userStoryInSprint2 = new UserStoryInSprint(
+                userStoryInSprintID2,
+                userStoryEffortEstimate,
+                userStoryInSprintStatus);
+
+        //Act
+        boolean result = userStoryInSprint1.equals(userStoryInSprint2);
+
+        //Assert
+        assertFalse(result);
+    }
+
+    /**
+     * This test ensure that objects from different classes are not equal.
+     */
+    @Test
+    @DisplayName("ensure object does not equal object of different class")
+    void objectDoesNotEqualProjectOfDifferentClass() {
+        //Arrange
+        UserStoryInSprint userStoryInSprint = new UserStoryInSprint(
+                userStoryInSprintID,
+                userStoryEffortEstimate,
+                userStoryInSprintStatus);
+        String fakeUserStoryInSprint = "I'm a fake!";
+
+        //Act
+        boolean result = userStoryInSprint.equals(fakeUserStoryInSprint);
+
+        //Assert
+        assertFalse(result);
+    }
+
+    /**
+     * This test ensures that 2 equal objects have the same hashcode.
+     */
+    @Test
+    @DisplayName("ensure equal objects have same hashcode")
+    void ensureEqualObjectsHaveSameHashcode() {
+        //Arrange
+        UserStoryInSprint userStoryInSprint1 = new UserStoryInSprint(
+                userStoryInSprintID,
+                userStoryEffortEstimate,
+                userStoryInSprintStatus);
+
+        UserStoryInSprint userStoryInSprint2 = new UserStoryInSprint(
+                userStoryInSprintID,
+                userStoryEffortEstimate,
+                userStoryInSprintStatus);
+
+        //Act
+        int hashCode1 = userStoryInSprint1.hashCode();
+        int hashCode2 = userStoryInSprint2.hashCode();
+
+        //Assert
+        assertEquals(hashCode1, hashCode2);
+    }
+
+    /**
+     * This test ensures that 2 different objects have different hashcodes.
+     */
+    @Test
+    @DisplayName("ensure different objects have different hashcodes")
+    void ensureDifferentObjectsHaveDifferentHashcodes() {
+        //Arrange
+        UserStoryInSprint userStoryInSprint1 = new UserStoryInSprint(
+                userStoryInSprintID,
+                userStoryEffortEstimate,
+                userStoryInSprintStatus);
+
+        UserStoryInSprintID userStoryInSprintID2 = mock(UserStoryInSprintID.class);
+        UserStoryInSprint userStoryInSprint2 = new UserStoryInSprint(
+                userStoryInSprintID2,
+                userStoryEffortEstimate,
+                userStoryInSprintStatus);
+
+        //Act
+        int hashCode1 = userStoryInSprint1.hashCode();
+        int hashCode2 = userStoryInSprint2.hashCode();
+
+        //Assert
+        assertNotEquals(hashCode1, hashCode2);
+    }
 }
