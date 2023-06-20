d4ea547af169f9bb984bd9b9522b5bc535ce3f68
Marisa Graça <1221849@isep.ipp.pt>
2023-06-20 13:18:54 +0100

    T608: Add comments with details about functions in listSprint.js  (Close #125)

--- ui/src/pages/listSprint.js
+++ ui/src/pages/listSprint.js
@@ -6,6 +6,7 @@ import {useParams, useNavigate, useLocation} from "react-router-dom";
 import AppContext from "../context/AppContext";
 import {fetchSprints} from "../context/Actions";
 
+// Array defining the headers for the table
 const headers = [
     { label: "Sprint Number", key: "sprintNumber"},
     { label: "Start Date", key: "startDate"},
@@ -15,28 +16,29 @@ const headers = [
 ];
 
 const ListSprints = () => {
-
+    // Accessing state and dispatch from the AppContext
     const {state, dispatch} = useContext(AppContext);
+    // Extracting the projectCode parameter from the URL
     const {projectCode} = useParams();
 
     //The effect scrolls the window to the top of the page, ensuring that the header and the top portion
     //of the content are visible when rendering the page.
     useEffect(() => {
         window.scrollTo(0, 0);
-
+        // Fetching the sprints for the specified projectCode
         fetchSprints(dispatch, projectCode)
     }, []);
-
+    // Obtaining the navigate function for programmatic navigation
     const navigate = useNavigate();
-
+    // Obtaining the current location object
     const location = useLocation();
-
+    // Filtering sprints for the specified projectCode
     const sprintInProject = state.sprints.data.filter((sprint) => sprint.projectCode === projectCode);
-
+    // Function to handle viewing the sprint backlog
     const handleViewSprintBacklog = (sprintNumber) => {
         navigate(`/sprintBacklog/${projectCode}/${sprintNumber}`);
     };
-
+    // Mapping sprints to a formatted array
     const sprints = sprintInProject.map((sprint) => ({
         sprintNumber: sprint.sprintNumber,
         startDate: sprint.startDate,
@@ -54,11 +56,12 @@ const ListSprints = () => {
     }));
 
 
-
+    // Function to handle creating a new sprint
     const handleCreateSprint = () => {
         const from = location.pathname;
         navigate(`/createSprint/${projectCode}`, { state: { from } });
     };
+    // Function to handle row click (navigate to the view project page)
     const handleRowClick = (id) => {
         navigate(`/viewProject/${id}`);
     };
